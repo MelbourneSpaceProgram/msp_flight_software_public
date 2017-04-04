80f7e9444837439f6f096c9c46826a5404d656ff
Anthony Kremor <anthony@kremor.com.au>
2017-04-04 22:35:25 +1000

    Finish schedule rewrite to C++.

---
+++ src/allocator/schedule.h
@@ -0,0 +1,17 @@
+/*
+ * schedule.h
+ *
+ *  Created on: 2Apr.,2017
+ *      Author: Anthony
+ */
+
+#ifndef SCHEDULE_H_
+#define SCHEDULE_H_
+
+
+#ifdef __cplusplus
+
+
+#endif
+
+#endif /* SCHEDULE_H_ */
