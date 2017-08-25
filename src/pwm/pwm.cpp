695280ced117cd7f6ec621363e538f0bc25b801e
Anthony Kremor <anthony@kremor.com.au>
2017-08-25 22:55:22 +1000

    Integrate PWM code into main codebase. Still need to work on the array that defines which pins are available for PWM, but this can be worked on later as needed.
    Integrate now to allow other teams to start testing against the code, so we can work out what, if anything, needs to be improved.

--- src/pwm/pwm.cpp
+++ src/pwm/pwm.cpp
@@ -7,7 +7,6 @@
  */
 
 // TODO: Get rid of redundant headers after code is working
-#include <ti/sysbios/knl/Task.h>
 #include <ti/drivers/PWM.h>
 #include "Board.h"
 #include <ti/drivers/GPIO.h>
@@ -15,36 +14,27 @@
 
 #include "pwm.hpp"
 
-
+PWM_Handle pwm[NUM_PWM];
+    PWM_Params params[NUM_PWM];
 // this is the core pwm function
 // TODO: Abstract once it is working
-void pwm_execute(void) {
+void MSP_pwm_init(void)
+{
 
-    int         pin_pwm[NUM_PWM], pin_led[NUM_PWM], pol[NUM_PWM], i; /* pwm pin , polarity pin, polarity */
-    uint16_t    period, duty[NUM_PWM];
-    uint32_t    sleep_period = SLEEP_PERIOD;
-    double      duty_percent[NUM_PWM];
+    int pin_pwm[NUM_PWM], i; /* pwm pin , polarity pin, polarity */
+    uint16_t period = PERIOD;
+    uint16_t duty[NUM_PWM];
+    double duty_percent[NUM_PWM];
 
-    PWM_Handle pwm[NUM_PWM];
-    PWM_Params params[NUM_PWM];
 
-    period = PERIOD;
+
     Board_initGPIO();
 
     /* getting pin and port input and initialising each pwm */
-    printf("Usage: pin_pwm    pin_led    initial_duty_percent\n");
-    for (i = 0; i < NUM_PWM; i++) {
-        printf("Enter information for pwm %d: ", i);
-        if (scanf("%d %d %lf", &pin_pwm[i], &pin_led[i], &duty_percent[i])
-                != NUM_INPUTS_1) {
-            printf("Incorrect number of inputs\n");
-            return;
-        }
-        if (duty_percent[i] < 0 || duty_percent[i] > 1) {
-            printf("Enter a valid duty percentage between 0 and 1\n");
-            return;
-        }
-
+    for (i = 0; i < NUM_PWM; i++)
+    {
+        pin_pwm[i] = i;
+        duty_percent[i] = 0;
         duty[i] = duty_in_usecs(duty_percent[i]);
 
         /* Sleep time in microseconds */
@@ -58,42 +48,25 @@ void pwm_execute(void) {
 
         pwm[i] = PWM_open(pin_pwm[i], &params[i]);
         /* refer to Note 1 */
-        if (pwm[i] == NULL) {
+        if (pwm[i] == NULL)
+        {
             /* pin1 didn't open */
-            printf("Error\n");
-            while (1);
+            // TODO Add error handling and log output
+            while (1)
+                ;
         }
-
-        /* initialising pin2 (high) */
-        pol[i] = 1;
 
         PWM_start(pwm[i]);
     }
-
-
-
-    /* Loop forever while getting a new duty at a particular frequency */
-    while (1) {
-        for (i = 0; i < NUM_PWM; i++) {
-            PWM_setDuty(pwm[i], duty_in_usecs(duty_percent[i]));
-            GPIO_write(pin_led[i], pol[i]);
-        }
-            //Task_sleep(sleep_period);
-            /* (determines how often duty_percent and polarity are redefined) */
-        printf("Usage: duty_percent    polarity\n");
-        for (i = 0; i < NUM_PWM; i++) {
-            printf("pwm %i: ", i);
-            if (scanf("%lf %d", &duty_percent[i], &pol[i]) != NUM_INPUTS_2) {
-                printf("Incorrect input\n");
-                return;
-            }
-        }
-    }
-
 }
 
+void MSP_PWM_set_duty(int index, double duty_cycle)
+{
+    PWM_setDuty(pwm[index], duty_in_usecs(duty_cycle));
+}
 
 // returns the duty in microseconds given the duty in percentage
-uint16_t duty_in_usecs(double duty_percent) {
+uint16_t duty_in_usecs(double duty_percent)
+{
     return PERIOD * duty_percent;
 }
