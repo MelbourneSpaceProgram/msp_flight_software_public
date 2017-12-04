413d2a88b62dc7e38d84d8c1501e1c5958c1f4b2
Benjamin Ding <bding.kabooshy@gmail.com>
2017-12-04 21:55:34 +1100

    Create accessors/mutators for PackWriter

--- src/CDH/util/PackWriter.h
+++ src/CDH/util/PackWriter.h
@@ -8,23 +8,31 @@
 #ifndef PACKWRITER_H_
 #define PACKWRITER_H_
 
+#define BUFFER_SIZE 256
+
 class PackWriter
 {
 public:
-    int packed_length;
-    char packed_message_buffer[256];
     enum PackMessageType {TEMPERATURE_SENSOR = 0,
-                                    RADIATION_SENSOR = 1};
+                                   RADIATION_SENSOR = 1};
     enum PackMessageVersion {V1 = 0};
     PackWriter(PackMessageType type, PackMessageVersion version);
     virtual ~PackWriter();
+    int getPackedLength();
+    char *getPackedMessageBuffer();
     template <class T>
     PackWriter& addData(T data) {
-        // TODO: Prevent overpacking
-        packed_length += sizeof(T);
-        std::memcpy(&packed_message_buffer + packed_length, &data, sizeof(T));
-        return *this;
+        if(sizeof(T) > BUFFER_SIZE - packed_length) {
+            // TODO: Throw and handle exception
+        } else {
+            packed_length += sizeof(T);
+            std::memcpy(&packed_message_buffer + packed_length, &data, sizeof(T));
+            return *this;
+        }
     }
+private:
+    int packed_length;
+    char packed_message_buffer[BUFFER_SIZE];
 };
 
 #endif /* PACKWRITER_H_ */
