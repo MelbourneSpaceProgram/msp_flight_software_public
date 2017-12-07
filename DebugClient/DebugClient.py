3861ac746b6b932ecf5623fb5dddada58177c0cf
Benjamin Ding <bding.kabooshy@gmail.com>
2017-12-08 00:14:22 +1100

    Implement write to log and read line
    Will need to implement corresponding newline write on MSP432

--- DebugClient/DebugClient.py
+++ DebugClient/DebugClient.py
@@ -1,5 +1,7 @@
-import time
-import serial
+import time, serial, io
+
+class Sensor:
+    Temperature, Radiation = range(2)
 
 userPort = input("Enter port (COM1):") or "COM1"
 userBaud = input("Enter baud rate (115200):") or 115200
@@ -10,7 +12,7 @@ ser.baudrate = userBaud
 ser.bytesize = serial.EIGHTBITS
 ser.parity = serial.PARITY_NONE
 ser.stopbits = serial.STOPBITS_ONE
-ser.timeout = 1
+ser.timeout = None      #TODO: Check if should have a timeout
 ser.xonxoff = False     #disable software flow control
 ser.rtscts = False     #disable hardware (RTS/CTS) flow control
 ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
@@ -22,16 +24,25 @@ except Exception as e:
     print("Error opening serial port: " + str(e))
     exit()
 
-print("Port " + ser.portstr + "will be used.")
+print("Port " + ser.portstr + " will be used.")
+
+f = open("msplog.txt", "a+")
+
 while ser.isOpen():
     try:
         ser.flushInput() #flush input buffer, discarding all its contents
         ser.flushOutput() #flush output buffer, aborting current output and discard all that is in buffer
         time.sleep(0.5)
+
+        bytesToRead = ser.inWaiting()
+        buf = ser.readline()
+        print(buf)
+        f.write(buf)
     except Exception as e:
         print("Error opening serial port:" + str(e))
+        f.close()
 
 print("Stop DebugClient")
-
+f.close()
 
 
