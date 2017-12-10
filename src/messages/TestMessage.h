49d17eb03f4ed08c7d1b6da2c5bc53f0f7009ca8
Benjamin Ding <bding.kabooshy@gmail.com>
2017-12-10 23:43:57 +1100

    Add requestcodes and start receive functionality

--- src/messages/TestMessage.h
+++ src/messages/TestMessage.h
@@ -10,15 +10,17 @@
 
 #include <src/messages/Message.h>
 #include <stdint.h>
+#include <vector>
+
+const uint8_t kTestMessageLength = 4;
 
 class TestMessage: public Message {
 public:
-    TestMessage(char* bytes, uint8_t size);
+    TestMessage(char bytes[kTestMessageLength]);
     virtual ~TestMessage();
     SerialisedMessage Serialise();
 private:
-    char* message_content;
-    uint8_t size;
+    char *message_content;
 };
 
 #endif /* TESTMESSAGE_H_ */
