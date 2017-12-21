b91505e2974049cb14df9715b0812c0c35c30267
Anthony Kremor <anthony@kremor.com.au>
2017-12-21 23:35:13 +1100

    Remove "logger" functionality, which will be replaced with the debugger and also the debug stream interface.
    The logger had significant impact on code size and encourages poor
    debugging techniques. It was also difficult to use and required a
    relatively large amount of boiler plate.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
+#include <pthread.h>
 #include <ti/sysbios/BIOS.h>
 #include <src/public_headers/init/init.hpp>
-#include <pthread.h>
-
 
 /* Example/Board Header files */
 #include "Board.h"
 
-
 int main(void) {
     init_core();
     init_time();
-    init_logger();
 
-    // All basic systems should now be operational. Start the main loop of the satellite.
+    // All basic systems should now be operational. Start the main loop of the
+    // satellite.
     init_satellite();
 
     // Start BIOS
--- src/public_headers/logger.h
+++
@@ -1,9 +0,0 @@
-#include <xdc/runtime/Log.h>   // For Log_warning1("Warning number #%d", 4); things
-#include <xdc/runtime/Diags.h> // For Log_print0(Diags_USER1, "hello"); things.
-
-#define log_print xdc_runtime_Log_print0
-#define log_error xdc_runtime_Log_error0
-#define log_warning xdc_runtime_Log_warning0
-#define log_info xdc_runtime_Log_info0
-#define log_put xdc_runtime_Log_put0
-#define log_write xdc_runtime_Log_write0
