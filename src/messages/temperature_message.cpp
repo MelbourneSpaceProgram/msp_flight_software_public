78291d67e64458a27f442bc16c0733f2366701cc
Benjamin Ding <bding.kabooshy@gmail.com>
2017-12-21 19:31:37 +1100

    Remove TemperatureMessage constructor stub

--- src/messages/temperature_message.cpp
+++ src/messages/temperature_message.cpp
@@ -1,10 +1,6 @@
 #include "src/messages/temperature_message.h"
 #include <src/CDH/util/serialised_message_builder.h>
 
-TemperatureMessage::TemperatureMessage() {
-    // Auto-generated constructor stub
-}
-
 TemperatureMessage::TemperatureMessage(float temperature, uint8_t timestamp,
                                        uint8_t sensor_id) {
     this->temperature = temperature;
