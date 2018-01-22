e2ca6e799097560ed2e2b379159abec8cfb393e1
Benjamin Ding <ben@dingbenjamin.com>
2018-01-22 20:27:48 +1100

    Enable ROV view for created tasks
    Refactor TaskHolder to use Task_create in place of Task_construct
    
    Untested for multiple tasks, but under the assumption that Task_create and Task_construct can be interchanged this should be safe.

--- src/tasks/task_holder.cpp
+++ src/tasks/task_holder.cpp
@@ -1,34 +1,31 @@
 #include <src/tasks/task_holder.h>
 #include <src/util/data_types.h>
 
-static Task_Struct kEmptyTaskStruct;
-
 TaskHolder::TaskHolder(uint16_t stack_size, char* name, int priority,
                        Runnable* runnable)
     : stack_size(stack_size),
       name(name),
       priority(priority),
-      stack(new byte[stack_size]),
       runnable(runnable),
-      task_struct(kEmptyTaskStruct),
       task_params(),
       handle(NULL) {}
 
-TaskHolder::~TaskHolder() { delete[] stack; }
+TaskHolder::~TaskHolder() {
+    // TODO(dingbenjamin): Implement safe stopping of contained runnable and
+    // task - ensure task is deleted and the task stack is freed.
+    // The runnable should be in charge of deciding how it safely stops
+}
 
 void TaskHolder::Init() {
     Task_Params_init(&task_params);
     task_params.stackSize = stack_size;
     task_params.instance->name = name;
-    task_params.stack = stack;
     task_params.priority = priority;
-
-    // TODO(dingbenjamin): Figure out initialisation of task struct
-    // TODO(dingbenjamin): Figure out last argument "xdc_runtime_Error_Block
-    // *__eb"
-    Task_construct(&task_struct, (Task_FuncPtr)runnable->GetRunnablePointer(),
-                   &task_params, NULL);
-    handle = Task_handle(&task_struct);
+    handle = Task_create((Task_FuncPtr)runnable->GetRunnablePointer(),
+                         &task_params, NULL);
+    if (handle == NULL) {
+        // TODO(dingbenjamin): Throw an error
+    }
 }
 
 Task_Handle TaskHolder::GetTaskHandle() const { return handle; }
