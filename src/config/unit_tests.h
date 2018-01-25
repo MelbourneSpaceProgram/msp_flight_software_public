a77eb8dd187979bca6f9a936490fb950a59fedff
Will Schuetz <willschuetz@outlook.com>
2018-01-25 20:17:57 +1000

    Add TEST_IGNORE to I2C hardware unit test

---
+++ src/config/unit_tests.h
@@ -0,0 +1,25 @@
+#ifndef SRC_CONFIG_UNIT_TESTS_H_
+#define SRC_CONFIG_UNIT_TESTS_H_
+
+/*
+
+   Usage
+
+   To ignore a Unity test, add the following to the beginning of the test
+   method in [name_of_functionality]_tests.cpp:
+
+   #ifdef [name_of_functionality]_TESTS_IGNORED
+       TEST_IGNORE_MESSAGE("some informative message")
+   #endif
+
+   It is also necessary add the following to this file:
+
+   #define [name_of_functionality]_TESTS_IGNORED
+
+   By default, hardware tests are ignored.
+
+ */
+
+#define I2C_TESTS_IGNORED
+
+#endif  //  SRC_CONFIG_UNIT_TESTS_H_
