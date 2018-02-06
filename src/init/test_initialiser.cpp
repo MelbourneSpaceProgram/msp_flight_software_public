9137a8a117ea41e8b1b716a95d1e1d4730adafb6
Benjamin Ding <ben@dingbenjamin.com>
2018-02-06 22:13:41 +1100

    Isolate unit test thread execution
    Unit tests will now run before all other tasks are
    initialised with exception of PostBiosInit (its caller)
    
    Unit tests will run in isolation and not compete with
    any other tasks for resources

--- src/init/test_initialiser.cpp
+++ src/init/test_initialiser.cpp
@@ -1,6 +1,30 @@
 #include <src/init/test_initialiser.h>
 #include <test_runners/master_runner.h>
 
-TestInitialiser::TestInitialiser() {}
+TestInitialiser* TestInitialiser::instance = NULL;
 
-fnptr TestInitialiser::GetRunnablePointer() { return &master_tests_thread; }
+TestInitialiser* TestInitialiser::GetInstance() {
+    if (instance == NULL) {
+        instance = new TestInitialiser();
+    }
+    return instance;
+}
+
+TestInitialiser::TestInitialiser() : test_finished(NULL) {}
+
+void TestInitialiser::InitSemaphore(Semaphore_Handle sem_handle) {
+    if (test_finished == NULL) {
+        test_finished = sem_handle;
+    }
+}
+
+void TestInitialiser::FinishTests() {
+    Semaphore_post(test_finished);
+}
+
+fnptr TestInitialiser::GetRunnablePointer() { return &RunUnitTests; }
+
+void TestInitialiser::RunUnitTests() {
+    master_tests_thread();
+    TestInitialiser::GetInstance()->FinishTests();
+}
