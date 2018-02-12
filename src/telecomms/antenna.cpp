42b13cc557430abd27708019c5c92af33afa9a80
Benjamin Ding <ben@dingbenjamin.com>
2018-02-12 19:56:28 +1100

    Fix Antenna for deployment test
    Remove sleep mode, use off mode
    Flip bit flags
    Add millisecond to clock tick conversion in TryHeater()
    Correct valid state logic in GetStatus
    remove ValidBitRate() comment

--- src/telecomms/antenna.cpp
+++ src/telecomms/antenna.cpp
@@ -1,4 +1,5 @@
 #include <src/telecomms/antenna.h>
+#include <ti/sysbios/knl/Clock.h>
 
 Antenna *Antenna::instance = NULL;
 
@@ -41,10 +42,9 @@ void Antenna::SafeDeploy() const {
 
     // TODO(dingbenjamin): Find a better way to get around passing const
     // commands as args without an explicit copy
-    byte copy_command_mode_sleep = Antenna::kCommandModeSleep;
+    byte copy_command_mode_off = Antenna::kCommandModeAllOff;
 
-    bus->PerformWriteTransaction(Antenna::kAddress, &copy_command_mode_sleep,
-                                 1);
+    bus->PerformWriteTransaction(Antenna::kAddress, &copy_command_mode_off, 1);
 }
 
 // Algorithm 2 from "Antenna Deployment tester.pdf" of CS1-DD-544
@@ -59,14 +59,14 @@ bool Antenna::TryHeater(const byte heat_command, uint32_t wait_time) const {
     // TODO(dingbenjamin): Find a better way to get around passing const
     // commands as args without an explicit copy
     byte copy_heat_command = heat_command;
-    byte copy_command_mode_sleep = Antenna::kCommandModeSleep;
+    byte copy_command_mode_off = Antenna::kCommandModeAllOff;
 
     if (initialised) {
         bus->PerformWriteTransaction(Antenna::kAddress, &copy_heat_command, 1);
-        Task_sleep(wait_time);
+        Task_sleep(wait_time * 1000 / Clock_tickPeriod);
         if (IsDoorsOpen()) {
             bus->PerformWriteTransaction(Antenna::kAddress,
-                                         &copy_command_mode_sleep, 1);
+                                         &copy_command_mode_off, 1);
             // TODO(wschuetz): Remove Task_sleep on overflow bug fix
             Task_sleep(100);
             return true;
@@ -91,12 +91,13 @@ void Antenna::InitAntenna(I2c *bus) {
 AntennaMessage Antenna::GetStatus() const {
     uint8_t read_buffer;
     bus->PerformReadTransaction(kAddress, &read_buffer, 1);
-    // TODO(dingbenjamin): Clarify with Stan from Endurosat what a 1/0 means
+    // Bit at the zero mask should ALWAYS be 0 as specified by data sheet
+    // Bits at the door masks should be 1 when doors opened, 0 otherwise
     AntennaMessage status((kZeroMask ^ read_buffer) & kZeroMask,
-                          (kDoorOneMask ^ read_buffer) & kDoorOneMask,
-                          (kDoorTwoMask ^ read_buffer) & kDoorTwoMask,
-                          (kDoorThreeMask ^ read_buffer) & kDoorThreeMask,
-                          (kDoorFourMask ^ read_buffer) & kDoorFourMask,
+                          (kDoorOneMask & read_buffer) & kDoorOneMask,
+                          (kDoorTwoMask & read_buffer) & kDoorTwoMask,
+                          (kDoorThreeMask & read_buffer) & kDoorThreeMask,
+                          (kDoorFourMask & read_buffer) & kDoorFourMask,
                           kStateMask & read_buffer);
     // TODO(wschuetz): Remove Task_sleep on overflow bug fix
     Task_sleep(100);
