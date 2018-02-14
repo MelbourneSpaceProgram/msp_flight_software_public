955cf9fb995dc0771e5dda1117fe19a43916c180
HUGO-SURFACE\Hugo <hhumbers@gmail.com>
2018-02-14 14:47:33 +1100

    Implement Hamming coder for database

---
+++ src/database/HammingCoder.h
@@ -0,0 +1,27 @@
+#ifndef SRC_DATABASE_HAMMING_CODER_H_
+#define SRC_DATABASE_HAMMING_CODER_H_
+
+#include <src/util/data_types.h>
+
+typedef struct HammingEncodedByte {
+    byte codewords[2];
+} HammingEncodedByte;
+
+typedef struct HammingDecodedByte {
+    byte data;
+    bool valid;
+} HammingDecodedByte;
+
+class HammingCoder {
+    //TODO(hugorilla): pass method arguments by constant reference (where appropriate)
+   public:
+    static HammingEncodedByte Encode(byte data);
+    static HammingDecodedByte Decode(HammingEncodedByte hamming_encoded_byte);
+
+   private:
+    static byte EncodeNibble(byte nibble);
+    static bool ValidateCodeword(byte& codeword);
+    static byte ExtractAndJoinData(byte c0, byte c1);
+};
+
+#endif  // SRC_DATABASE_HAMMING_CODER_H_
