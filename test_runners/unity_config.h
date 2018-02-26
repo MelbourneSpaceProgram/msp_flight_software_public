3627cc9adc064730972d1ea483e64580f1440aea
MelbourneSpaceSteward <information@melbournespace.org>
2018-02-26 14:16:04 +1100

    Add and enable unity_config.h

---
+++ test_runners/unity_config.h
@@ -0,0 +1,9 @@
+/* Set compiler flag --define=UNITY_INCLUDE_CONFIG_H to activate */
+
+#ifndef UNITY_CONFIG_H
+#define UNITY_CONFIG_H
+
+#define UNITY_INCLUDE_FLOAT
+#define UNITY_INCLUDE_DOUBLE
+
+#endif /* UNITY_CONFIG_H */
