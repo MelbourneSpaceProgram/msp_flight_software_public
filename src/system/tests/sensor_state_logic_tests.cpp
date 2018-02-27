b2b2b4ef69aa34795f1e58410f2736b4f1d63fa7
Will Schuetz <willschuetz@outlook.com>
2018-02-28 10:33:27 +1100

    Adds the state flow framework

--- src/system/tests/sensor_state_logic_tests.cpp
+++ src/system/tests/sensor_state_logic_tests.cpp
@@ -13,11 +13,11 @@ static const float kTwoEclipseCapacity = 0.2;
 static const float kThreePassCapacity = 0.4;
 static const float kHysteresis = 0.02;
 
-static const uint8_t kTempBatteryOperationalLow = 5;
-static const uint8_t kTempBatteryOperationalHigh = 40;
-static const uint8_t kHysteresisTemp = 1;
+static const double kTempBatteryOperationalLow = 5;
+static const double kTempBatteryOperationalHigh = 40;
+static const double kHysteresisTemp = 1;
 
-static const uint8_t kTempTelecomsOperationalMax = 80;
+static const double kTempTelecomsOperationalMax = 80;
 
 StateId BatteryChargeStateCheck(double soc, StateId current_state) {
     switch (current_state) {
@@ -83,102 +83,102 @@ StateId TelecomsTempStateCheck(double temp, StateId current_state) {
 
 void TestBatteryChargeStateFlow(void) {
     StateManager* state_manager = StateManager::GetStateManager();
-    BatteryChargeStateMachine battery_charge_sm(state_manager);
+    BatteryChargeStateMachine battery_charge_state_machine(state_manager);
 
     TestI2cSensor battery_soc_sensor("battery_soc_sensor");
-    battery_charge_sm.RegisterWithSensor(&battery_soc_sensor);
+    battery_charge_state_machine.RegisterWithSensor(&battery_soc_sensor);
 
-    uint8_t MAX_SOC = 1;
-    uint8_t MIN_SOC = 0.9;
-    double dummy_soc_reading = MIN_SOC;
+    double kMaxSoc = 1;
+    double kMinSoc = 0.9;
+    double dummy_soc_reading = kMinSoc;
     StateId state_before_reading;
-    while (dummy_soc_reading < MAX_SOC) {
-        state_before_reading = battery_charge_sm.GetCurrentState();
+    while (dummy_soc_reading < kMaxSoc) {
+        state_before_reading = battery_charge_state_machine.GetCurrentState();
         battery_soc_sensor.SetDummySensorData(dummy_soc_reading);
         battery_soc_sensor.TakeReading();
 
         TEST_ASSERT_EQUAL_INT(
             BatteryChargeStateCheck(dummy_soc_reading, state_before_reading),
-            battery_charge_sm.GetCurrentState());
+            battery_charge_state_machine.GetCurrentState());
         dummy_soc_reading += 0.01;
     }
 
-    while (dummy_soc_reading > MIN_SOC) {
-        state_before_reading = battery_charge_sm.GetCurrentState();
+    while (dummy_soc_reading > kMinSoc) {
+        state_before_reading = battery_charge_state_machine.GetCurrentState();
         battery_soc_sensor.SetDummySensorData(dummy_soc_reading);
         battery_soc_sensor.TakeReading();
 
         TEST_ASSERT_EQUAL_INT(
             BatteryChargeStateCheck(dummy_soc_reading, state_before_reading),
-            battery_charge_sm.GetCurrentState());
+            battery_charge_state_machine.GetCurrentState());
         dummy_soc_reading -= 0.01;
     }
 }
 
 void TestBatteryTempStateFlow(void) {
     StateManager* state_manager = StateManager::GetStateManager();
-    BatteryTempStateMachine battery_temp_sm(state_manager);
+    BatteryTempStateMachine battery_temp_state_machine(state_manager);
 
     TestI2cSensor battery_temp_sensor("battery_temp_sensor");
-    battery_temp_sm.RegisterWithSensor(&battery_temp_sensor);
+    battery_temp_state_machine.RegisterWithSensor(&battery_temp_sensor);
 
-    uint8_t MAX_TEMP = 50;
-    uint8_t MIN_TEMP = 0;
-    double dummy_temp_reading = MIN_TEMP;
+    double kMaxTemp = 50;
+    double kMinTemp = 0;
+    double dummy_temp_reading = kMinTemp;
     StateId state_before_reading;
-    while (dummy_temp_reading < MAX_TEMP) {
-        state_before_reading = battery_temp_sm.GetCurrentState();
+    while (dummy_temp_reading < kMaxTemp) {
+        state_before_reading = battery_temp_state_machine.GetCurrentState();
         battery_temp_sensor.SetDummySensorData(dummy_temp_reading);
         battery_temp_sensor.TakeReading();
 
         TEST_ASSERT_EQUAL_INT(
             BatteryTempStateCheck(dummy_temp_reading, state_before_reading),
-            battery_temp_sm.GetCurrentState());
+            battery_temp_state_machine.GetCurrentState());
         dummy_temp_reading += 1;
     }
 
-    while (dummy_temp_reading > MIN_TEMP) {
-        state_before_reading = battery_temp_sm.GetCurrentState();
+    while (dummy_temp_reading > kMinTemp) {
+        state_before_reading = battery_temp_state_machine.GetCurrentState();
         battery_temp_sensor.SetDummySensorData(dummy_temp_reading);
         battery_temp_sensor.TakeReading();
 
         TEST_ASSERT_EQUAL_INT(
             BatteryTempStateCheck(dummy_temp_reading, state_before_reading),
-            battery_temp_sm.GetCurrentState());
+            battery_temp_state_machine.GetCurrentState());
         dummy_temp_reading -= 1;
     }
 }
 
 void TestTelecomsTempStateFlow(void) {
     StateManager* state_manager = StateManager::GetStateManager();
-    TelecomsTempStateMachine telecoms_temp_sm(state_manager);
+    TelecomsTempStateMachine telecoms_temp_state_machine(state_manager);
 
     TestI2cSensor telecoms_temp_sensor("telecoms_temp_sensor");
-    telecoms_temp_sm.RegisterWithSensor(&telecoms_temp_sensor);
+    telecoms_temp_state_machine.RegisterWithSensor(&telecoms_temp_sensor);
 
-    uint8_t MAX_TEMP = kTempTelecomsOperationalMax + 10;
-    uint8_t MIN_TEMP = 78;
-    double dummy_temp_reading = MIN_TEMP;
+    double kMaxTemp = kTempTelecomsOperationalMax + 10;
+    double kMinTemp = 78;
+    double dummy_temp_reading = kMinTemp;
     StateId state_before_reading;
-    while (dummy_temp_reading < MAX_TEMP) {
-        state_before_reading = telecoms_temp_sm.GetCurrentState();
+    while (dummy_temp_reading < kMaxTemp) {
+        state_before_reading = telecoms_temp_state_machine.GetCurrentState();
         telecoms_temp_sensor.SetDummySensorData(dummy_temp_reading);
         telecoms_temp_sensor.TakeReading();
 
         TEST_ASSERT_EQUAL_INT(
             TelecomsTempStateCheck(dummy_temp_reading, state_before_reading),
-            telecoms_temp_sm.GetCurrentState());
+            telecoms_temp_state_machine.GetCurrentState());
         dummy_temp_reading += 1;
     }
 
-    while (dummy_temp_reading > MIN_TEMP) {
-        state_before_reading = telecoms_temp_sm.GetCurrentState();
+    while (dummy_temp_reading > kMinTemp) {
+        state_before_reading = telecoms_temp_state_machine.GetCurrentState();
         telecoms_temp_sensor.SetDummySensorData(dummy_temp_reading);
         telecoms_temp_sensor.TakeReading();
 
         TEST_ASSERT_EQUAL_INT(
             TelecomsTempStateCheck(dummy_temp_reading, state_before_reading),
-            telecoms_temp_sm.GetCurrentState());
+            telecoms_temp_state_machine.GetCurrentState());
         dummy_temp_reading -= 1;
     }
 }
