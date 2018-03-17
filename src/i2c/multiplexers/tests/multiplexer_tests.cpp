01fe15bbc7779af338a38ed1371fd84475f99a0e
Anthony Kremor <github@anthonykremor.com>
2018-03-17 19:51:38 +1100

    Update I2C adresses to enable FlatSat testing

--- src/i2c/multiplexers/tests/multiplexer_tests.cpp
+++ src/i2c/multiplexers/tests/multiplexer_tests.cpp
@@ -8,8 +8,8 @@
 #include <ti/sysbios/knl/Task.h>
 #include <src/sensors/i2c_sensors/mcp9808.hpp>
 
-static const byte kMultiplexerAddress = 0x74;
-static const byte kTempSensorAddress = 0x18;
+static const byte kMultiplexerAddress = 0x76;
+static const byte kTempSensorAddress = 0x1A;
 static const byte kAdcAddress = 0x48;
 static const byte kAdcConversionRegisterLocation = 0x00;
 static const double kRoomTemperature = 25;
@@ -19,7 +19,7 @@ void TestMultiplexer(void) {
 #ifdef MULTIPLEXER_TEST_IGNORED
     TEST_IGNORE_MESSAGE("Hardware test ignored");
 #endif
-    I2c bus(Board_I2C_TMP);
+    I2c bus(I2C_BUS_A);
     I2cMultiplexer multiplexer(&bus, kMultiplexerAddress);
     MCP9808 temp_sensor(&bus, kTempSensorAddress, "temp sensor");
     MCP9808 temp_sensor2(&bus, kTempSensorAddress, "temp sensor");
