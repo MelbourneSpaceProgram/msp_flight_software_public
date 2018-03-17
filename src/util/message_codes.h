1c157d775e5bb067a98403de627449fbc88033c5
Anthony Kremor <github@anthonykremor.com>
2018-03-18 00:12:33 +1100

    Define more nanopb messages

--- src/util/message_codes.h
+++ src/util/message_codes.h
@@ -14,6 +14,14 @@ enum DebugMessageType {
     kPopMessageQueue = 4,
     kAntenna = 5,
 
+    kBms1InputCurrentReadingCode = 0x01,
+    kBms1InputVoltageReadingCode = 0x02,
+    kPrimaryMcuRegulatorCurrentReadingCode = 0x03,
+    kMagnetorquerXCurrentReadingCode = 0x04,
+    kAdcsSystemStateReadingCode = 0x05,
+    kMagnetometerReadingRequestCode = 0x06,
+    kTorqueOutputReadingCode = 0x07,
+    kMagnetometerReadingCode = 0x08,
     kTestSensorReadingCode = 0x09,
     kTestRequestCode = 0x0A
 };
