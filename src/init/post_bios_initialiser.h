636fcba9bb9bd8c5b56cc9da6d19f532a01103af
Benjamin Ding <ben@dingbenjamin.com>
2018-03-25 18:45:08 +0200

    Refactor PostBiosInitialiser
    Remove TasksInit() as it was fairly pointless
    Clean the main PostBiosInit() function

--- src/init/post_bios_initialiser.h
+++ src/init/post_bios_initialiser.h
@@ -1,6 +1,7 @@
 #ifndef SRC_INIT_POST_BIOS_INITIALISER_H_
 #define SRC_INIT_POST_BIOS_INITIALISER_H_
 
+#include <src/i2c/i2c.h>
 #include <src/tasks/runnable.h>
 
 class PostBiosInitialiser : public Runnable {
@@ -11,6 +12,11 @@ class PostBiosInitialiser : public Runnable {
    private:
     // TODO(dingbenjamin): Suppress unused function warning
     static void PostBiosInit();
+    static void InitSingletons(I2c* antenna_bus);
+    static void InitRadioListener();
+    static void InitStateManagement();
+    static void InitDataDashboard();
+    static void RunUnitTests();
 };
 
 #endif  // SRC_INIT_POST_BIOS_INITIALISER_H_
