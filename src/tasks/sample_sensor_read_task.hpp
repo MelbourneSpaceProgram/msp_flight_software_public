7776b36eada3566b563d94bb13d30b89acb73e7d
Benjamin Ding <ben@dingbenjamin.com>
2018-03-25 15:05:34 +0200

    Remove old sample sensor read tasks

--- src/tasks/sample_sensor_read_task.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SRC_TASKS_SAMPLE_SENSOR_READ_TASK_HPP_
-#define SRC_TASKS_SAMPLE_SENSOR_READ_TASK_HPP_
-
-#include <src/sensors/i2c_sensors/i2c_sensor.h>
-
-void InitSampleSensorReadTask();
-
-#endif  // SRC_TASKS_SAMPLE_SENSOR_READ_TASK_HPP_
