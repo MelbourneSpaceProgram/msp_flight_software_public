379325b68c2a7b8a79072d359c4d5690c28c54db
Benjamin Ding <ben@dingbenjamin.com>
2018-03-27 16:06:07 +0200

    Add constness to I2cMultiplexer

--- src/i2c/multiplexers/i2c_multiplexer.h
+++ src/i2c/multiplexers/i2c_multiplexer.h
@@ -5,27 +5,27 @@
 
 class I2cMultiplexer {
    public:
-    I2cMultiplexer(I2c* bus, byte address);
-    I2c* GetBus();
+    I2cMultiplexer(const I2c* bus, byte address);
+    const I2c* GetBus() const;
     byte GetAddress() const;
-    void OpenChannel(uint8_t channel);
-    void CloseChannel(uint8_t channel);
-    void CloseAllChannels();
-    byte GetChannelStates();
+    void OpenChannel(uint8_t channel) const;
+    void CloseChannel(uint8_t channel) const;
+    void CloseAllChannels() const;
+    byte GetChannelStates() const;
     enum MuxChannels {
-        kMuxChannel0= 0x00,
-        kMuxChannel1= 0x01,
-        kMuxChannel2= 0x02,
-        kMuxChannel3= 0x04,
-        kMuxChannel4= 0x08,
-        kMuxChannel5= 0x10,
-        kMuxChannel6= 0x20,
-        kMuxChannel7= 0x40
+        kMuxChannel0 = 0x00,
+        kMuxChannel1 = 0x01,
+        kMuxChannel2 = 0x02,
+        kMuxChannel3 = 0x04,
+        kMuxChannel4 = 0x08,
+        kMuxChannel5 = 0x10,
+        kMuxChannel6 = 0x20,
+        kMuxChannel7 = 0x40
     };
 
    private:
-    I2c* bus;
-    byte address;
+    const I2c* bus;
+    const byte address;
 };
 
 #endif  // SRC_I2C_MULTIPLEXERS_I2C_MULTIPLEXER_H_
