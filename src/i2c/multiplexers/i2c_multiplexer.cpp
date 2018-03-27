cb8e5a900045e4ff025b20d8a12563bc3890c257
Benjamin Ding <ben@dingbenjamin.com>
2018-03-27 16:06:07 +0200

    Add constness to I2cMultiplexer

--- src/i2c/multiplexers/i2c_multiplexer.cpp
+++ src/i2c/multiplexers/i2c_multiplexer.cpp
@@ -1,30 +1,30 @@
 #include <src/i2c/multiplexers/i2c_multiplexer.h>
 
-I2cMultiplexer::I2cMultiplexer(I2c* bus, byte address)
+I2cMultiplexer::I2cMultiplexer(const I2c* bus, byte address)
     : bus(bus), address(address) {}
 
-I2c* I2cMultiplexer::GetBus() { return bus; }
+const I2c* I2cMultiplexer::GetBus() const { return bus; }
 
 byte I2cMultiplexer::GetAddress() const { return address; }
 
-void I2cMultiplexer::OpenChannel(uint8_t channel) {
+void I2cMultiplexer::OpenChannel(uint8_t channel) const {
     byte write_buffer = GetChannelStates();
-    write_buffer |=  (1 << channel);
+    write_buffer |= (1 << channel);
     GetBus()->PerformWriteTransaction(GetAddress(), &write_buffer, 1);
 }
 
-void I2cMultiplexer::CloseChannel(uint8_t channel) {
+void I2cMultiplexer::CloseChannel(uint8_t channel) const {
     byte write_buffer = GetChannelStates();
-    write_buffer &=  ~(1 << channel);
+    write_buffer &= ~(1 << channel);
     GetBus()->PerformWriteTransaction(GetAddress(), &write_buffer, 1);
 }
 
-void I2cMultiplexer::CloseAllChannels() {
+void I2cMultiplexer::CloseAllChannels() const {
     byte write_buffer = 0;
     GetBus()->PerformWriteTransaction(GetAddress(), &write_buffer, 1);
 }
 
-byte I2cMultiplexer::GetChannelStates() {
+byte I2cMultiplexer::GetChannelStates() const {
     byte read_buffer;
     GetBus()->PerformReadTransaction(GetAddress(), &read_buffer, 1);
     return read_buffer;
