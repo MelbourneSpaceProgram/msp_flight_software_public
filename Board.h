a9d21d8e359c47f9b1835409f82083368dcdd9e1
Anthony Kremor <github@anthonykremor.com>
2018-03-27 23:51:46 +1100

    Remove unused HAL features

--- Board.h
+++ Board.h
@@ -50,30 +50,11 @@ extern "C" {
 
 #define Board_initGeneral           MSP_EXP432P401R_initGeneral
 
-#define Board_ADC0                  MSP_EXP432P401R_ADC0
-#define Board_ADC1                  MSP_EXP432P401R_ADC1
-
-#define Board_ADCBUF0               MSP_EXP432P401R_ADCBUF0
-#define Board_ADCBUF0CHANNEL0       MSP_EXP432P401R_ADCBUF0CHANNEL0
-#define Board_ADCBUF0CHANNEL1       MSP_EXP432P401R_ADCBUF0CHANNEL1
-
 #define Board_CAPTURE0              MSP_EXP432P401R_CAPTURE_TA1
 #define Board_CAPTURE1              MSP_EXP432P401R_CAPTURE_TA2
 #define Board_CAPTURE2              MSP_EXP432P401R_CAPTURE_TA3
 
-#define Board_GPIO_LED_ON           MSP_EXP432P401R_GPIO_LED_ON
-#define Board_GPIO_LED_OFF          MSP_EXP432P401R_GPIO_LED_OFF
-
-#define Board_GPIO_LED0             MSP_EXP432P401R_GPIO_LED1
-#define Board_GPIO_LED1             MSP_EXP432P401R_GPIO_LED_RED
-#define Board_GPIO_LED2             MSP_EXP432P401R_GPIO_LED_GREEN
-#define Board_GPIO_LED3             MSP_EXP432P401R_GPIO_LED_BLUE
-
-#define Board_GPIO_BUTTON0          MSP_EXP432P401R_GPIO_S1
-#define Board_GPIO_BUTTON1          MSP_EXP432P401R_GPIO_S2
-
 #define Board_I2C_TMP               I2C_BUS_A
-#define Board_I2C0                  I2C_BUS_A
 
 #define Board_PWM0                  MSP_EXP432P401R_PWM_TA1_1
 #define Board_PWM1                  MSP_EXP432P401R_PWM_TA1_2
@@ -84,55 +65,19 @@ extern "C" {
 #define Board_TIMER3                MSP_EXP432P401R_TIMER_TA_2
 #define Board_TIMER4                MSP_EXP432P401R_TIMER_TA_3
 
-#define Board_UART0                 MSP_EXP432P401R_UARTA0
-#define Board_UART1                 MSP_EXP432P401R_UARTA2
-
 #define Board_WATCHDOG0             MSP_EXP432P401R_WATCHDOG
-
-/* Board specific I2C addresses */
-#define Board_TMP_ADDR              (0x40)
-#define Board_SENSORS_BP_TMP_ADDR   Board_TMP_ADDR
-#define Board_TPL0401_ADDR          (0x40)
 
 /*
  * These macros are provided for backwards compatibility.
  * Please use the <Driver>_init functions directly rather
  * than Board_init<Driver>.
  */
-#define Board_initADC               ADC_init
-#define Board_initADCBuf            ADCBuf_init
 #define Board_initGPIO              GPIO_init
 #define Board_initI2C               I2C_init
 #define Board_initPWM               PWM_init
 #define Board_initSPI               SPI_init
 #define Board_initUART              UART_init
 #define Board_initWatchdog          Watchdog_init
-
-/*
- * These macros are provided for backwards compatibility.
- * Please use the corresponding 'Board_GPIO_xxx' macros as the macros
- * below are deprecated.
- */
-#define Board_ADCBUFCHANNEL0        Board_ADCBUF0CHANNEL0
-#define Board_ADCBUFCHANNEL1        Board_ADCBUF0CHANNEL1
-#define Board_BUTTON0               Board_GPIO_BUTTON0
-#define Board_BUTTON1               Board_GPIO_BUTTON1
-#define Board_Capture0              Board_CAPTURE0
-#define Board_Capture1              Board_CAPTURE1
-#define Board_Capture2              Board_CAPTURE2
-#define Board_LED_ON                Board_GPIO_LED_ON
-#define Board_LED_OFF               Board_GPIO_LED_OFF
-#define Board_LED0                  Board_GPIO_LED0
-#define Board_LED1                  Board_GPIO_LED1
-#define Board_LED2                  Board_GPIO_LED2
-#define Board_LED3                  Board_GPIO_LED3
-#define Board_RF430CL330_ADDR       (0x28)
-#define Board_Timer0                Board_TIMER0
-#define Board_Timer1                Board_TIMER1
-#define Board_Timer2                Board_TIMER2
-#define Board_Timer3                Board_TIMER3
-#define Board_Timer4                Board_TIMER4
-#define Board_TMP006_ADDR           Board_TMP_ADDR
 
 #ifdef __cplusplus
 }
--- MSP_EXP432P401R.h
+++ MSP_EXP432P401R.h
@@ -49,42 +49,6 @@
 extern "C" {
 #endif
 
-/* LEDs on MSP_EXP432P401R are active high. */
-#define MSP_EXP432P401R_GPIO_LED_OFF (0)
-#define MSP_EXP432P401R_GPIO_LED_ON (1)
-
-/*!
- *  @def    MSP_EXP432P401R_ADCName
- *  @brief  Enum of ADC channels on the MSP_EXP432P401R dev board
- */
-typedef enum MSP_EXP432P401R_ADCName {
-    MSP_EXP432P401R_ADC0 = 0,
-    MSP_EXP432P401R_ADC1,
-
-    MSP_EXP432P401R_ADCCOUNT
-} MSP_EXP432P401R_ADCName;
-
-/*!
- *  @def    MSP_EXP432P401R_ADCBufName
- *  @brief  Enum of ADC hardware peripherals on the MSP_EXP432P401R dev board
- */
-typedef enum MSP_EXP432P401R_ADCBufName {
-    MSP_EXP432P401R_ADCBUF0 = 0,
-
-    MSP_EXP432P401R_ADCBUFCOUNT
-} MSP_EXP432P401R_ADCBufName;
-
-/*!
- *  @def    MSP_EXP432P401R_ADCBuf0ChannelName
- *  @brief  Enum of ADCBuf channels on the MSP_EXP432P401R dev board
- */
-typedef enum MSP_EXP432P401R_ADCBuf0ChannelName {
-    MSP_EXP432P401R_ADCBUF0CHANNEL0 = 0,
-    MSP_EXP432P401R_ADCBUF0CHANNEL1,
-
-    MSP_EXP432P401R_ADCBUF0CHANNELCOUNT
-} MSP_EXP432P401R_ADCBuf0ChannelName;
-
 /*!
  *  @def    MSP_EXP432P401R_CaptureName
  *  @brief  Enum of Capture timer names on the MSP_EXP432P401R dev board
@@ -127,10 +91,6 @@ typedef enum Board_GPIOName {
  *  @def    MSP_EXP432P401R_I2CName
  *  @brief  Enum of I2C names on the MSP_EXP432P401R dev board
  */
-/*!
- *  @def    MSP_EXP432P401R_I2CName
- *  @brief  Enum of I2C names on the MSP_EXP432P401R dev board
- */
 typedef enum Board_I2CName {
     I2C_BUS_A = 0,
     I2C_BUS_B = 1,
@@ -138,16 +98,6 @@ typedef enum Board_I2CName {
     I2C_BUS_D = 3,
     Board_I2CCOUNT
 } Board_I2CName;
-
-/*!
- *  @def    MSP_EXP432P401R_I2CSlaveName
- *  @brief  Enum of I2CSlave names on the MSP_EXP432P401R dev board
- */
-typedef enum MSP_EXP432P401R_I2CSlaveName {
-    MSP_EXP432P401R_I2CSLAVEB0 = 0,
-
-    MSP_EXP432P401R_I2CSLAVECOUNT
-} MSP_EXP432P401R_I2CSlaveName;
 
 /*!
  *  @def    MSP_EXP432P401R_PWMName
@@ -185,15 +135,6 @@ typedef enum MSP_EXP432P401R_TimerName {
  *  @def    MSP_EXP432P401R_UARTName
  *  @brief  Enum of UART names on the MSP_EXP432P401R dev board
  */
-#ifndef SATELLITE_CONFIG
-typedef enum MSP_EXP432P401R_UARTName {
-    MSP_EXP432P401R_UARTA0 = 0,
-    MSP_EXP432P401R_UARTA2,
-
-    MSP_EXP432P401R_UARTCOUNT
-} MSP_EXP432P401R_UARTName;
-#endif
-
 typedef enum Board_UARTName {
       UART_MCU_MCU = 0,
       UART_CDH_UMB = 1,
