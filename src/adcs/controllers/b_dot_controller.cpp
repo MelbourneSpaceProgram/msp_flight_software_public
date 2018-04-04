43d649390f194d87a315bff4c28e89ef52e3bc76
Rowan Skewes <rowan.skewes@gmail.com>
2018-04-05 02:31:23 +1000

    Add constants for length of geomagnetic field vector

--- src/adcs/controllers/b_dot_controller.cpp
+++ src/adcs/controllers/b_dot_controller.cpp
@@ -1,11 +1,13 @@
 #include <src/adcs/controllers/b_dot_controller.h>
+#include <src/util/physical_constants.h>
 
 const double BDotController::max_achievable_dipole[] = {0.35, 0.35, 0.05};
 
 void BDotController::Control(const Matrix &b, const Matrix &b_dot,
                              Matrix &torque_output) {
     if (!b_dot.SameSize(b) || !b_dot.SameSize(torque_output) ||
-        b_dot.GetNRows() != 3 || b_dot.GetNColumns() != 1) {
+        b_dot.GetNRows() != geomagnetic_field_vector_nrows ||
+        b_dot.GetNColumns() != geomagnetic_field_vector_ncolumns) {
         etl::exception e("BDotController::Control arguments not 3x1",
                          "__FILE__", __LINE__);
         throw e;
---
+++ src/util/physical_constants.cpp
@@ -0,0 +1,4 @@
+#include <src/util/physical_constants.h>
+
+uint8_t geomagnetic_field_vector_nrows = 3;
+uint8_t geomagnetic_field_vector_ncolumns = 1;
