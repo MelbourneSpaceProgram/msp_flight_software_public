d5f83d03a95ed00ba24f321d4d8e25c58411490d
Rowan Skewes <rowan.skewes@gmail.com>
2018-04-05 02:36:10 +1000

    Convert data to byte arrays before writing to memcached

--- DebugClient/StandinMemcachedPopulator.py
+++ DebugClient/StandinMemcachedPopulator.py
@@ -1,6 +1,7 @@
 from pymemcache.client.base import Client
 import time
 import numpy as np
+import struct
 
 
 def rotate_z(point, angle):
@@ -15,27 +16,35 @@ def rotate_z(point, angle):
 if __name__ == "__main__":
 
     # Connect to memcached process
-    mc = Client(('localhost', 11211))
+    mc = Client(('localhost', 11211), allow_unicode_keys=True)
 
     magnetometer_reference = (0, 1, 1)
-    while(1):
-        now = time.time()
-        now_mag = rotate_z(magnetometer_reference,now)
-        mc.set("Simulation_Magnetometer_X", now_mag[0])
-        mc.set("Simulation_Magnetometer_Y", now_mag[1])
-        mc.set("Simulation_Magnetometer_Z", now_mag[2])
 
-        print("Magnetometer data being fed in:")
-        print(float(mc.get("Simulation_Magnetometer_X")))
-        print(float(mc.get("Simulation_Magnetometer_Y")))
-        print(float(mc.get("Simulation_Magnetometer_Z")))
+    while(1):
+        #mc.set(b"Simulation_Torque_X",
+        #    struct.pack('>d',0.01))
+        #mc.set(b"Simulation_Torque_Y",
+        #    struct.pack('>d',0.02))
+        #mc.set(b"Simulation_Torque_Z",
+        #    struct.pack('>d',0.03))
+
+        #mc.set("Simulation_Magnetometer_X",
+        #    struct.pack('>d',1))
+        #mc.set("Simulation_Magnetometer_Y",
+        #    struct.pack('>d',2))
+        #mc.set("Simulation_Magnetometer_Z",
+        #    struct.pack('>d',3))
+
+        if mc.get("Simulation_Magnetometer_X"):
+            print("Magnetometer data being fed in:")
+            print(struct.unpack('>d', mc.get("Simulation_Magnetometer_X"))[0])
+            print(struct.unpack('>d', mc.get("Simulation_Magnetometer_Y"))[0])
+            print(struct.unpack('>d', mc.get("Simulation_Magnetometer_Z"))[0])
 
         if mc.get("Simulation_Torque_X"):
             print("Torque output from flight computer:")
-            print(float(mc.get("Simulation_Torque_X")))
-            print(float(mc.get("Simulation_Torque_Y")))
-            print(float(mc.get("Simulation_Torque_Z")))
-        else:
-            print("No torque input yet")
+            print(struct.unpack('>d', mc.get("Simulation_Torque_X"))[0])
+            print(struct.unpack('>d', mc.get("Simulation_Torque_Y"))[0])
+            print(struct.unpack('>d', mc.get("Simulation_Torque_Z"))[0])
 
-        time.sleep(0.5)
+        time.sleep(0.1)
