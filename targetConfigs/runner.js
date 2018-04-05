dea9134b507aa84109b5508dbde101c1f8ac5331
Anthony Kremor <github@anthonykremor.com>
2018-04-06 09:23:48 +1000

    Add ADCS Sim launchpad configuration

--- targetConfigs/runner.js
+++ targetConfigs/runner.js
@@ -2,7 +2,6 @@ importPackage(Packages.com.ti.debug.engine.scripting)
 importPackage(Packages.com.ti.ccstudio.scripting.environment)
 importPackage(Packages.java.lang)
 
-
 // Must end with slash
 var projectRoot = "/tmp/code/";
 var logFilePath = "/tmp/code/out.txt";
@@ -12,10 +11,22 @@ script.setScriptTimeout(100 * 1000) // 100 seconds
 
 script.traceSetConsoleLevel(TraceLevel.INFO)
 
+// Valid arguments are either unittest or ditl
+var test_type = 'unittest';
+if(arguments.length != 0){
+	test_type = arguments[0];
+}
+
+script.traceWrite("Running loader for type: " + test_type);
 script.traceWrite("Starting MSP432")
 
 var debugServer = script.getServer("DebugServer.1");
-debugServer.setConfig( projectRoot + "targetConfigs/auto_flatsat.ccxml")
+
+if(test_type === 'unittest'){
+	debugServer.setConfig( projectRoot + "targetConfigs/auto_flatsat.ccxml")
+} else if (test_type === 'ditl'){
+	debugServer.setConfig( projectRoot + "targetConfigs/adcs_sim.ccxml")
+}
 
 var debugSession = debugServer.openSession(".*CORTEX.*");
 debugSession.target.connect()
@@ -27,7 +38,19 @@ var loaderExitBp = debugSession.breakpoint.add(loaderExit);
 debugSession.beginCIOLogging(logFilePath);
 
 debugSession.target.restart()
-debugSession.target.run()
+if(test_type === 'unittest'){
+	script.traceWrite("Running synch")
+	debugSession.target.run()
+} else if (test_type === 'ditl'){
+	script.traceWrite("Running asynch")
+	debugSession.target.runAsynch();
+	script.traceWrite("Disconnecting from MSP432");
+	debugSession.target.disconnect();
+	debugSession.endCIOLogging();
+
+	debugSession.terminate();
+	debugServer.stop();
+}
 
 nPC = debugSession.expression.evaluate("PC")
 
@@ -37,7 +60,6 @@ if (nPC == loaderExit) {
 	script.traceEnd()
 }
 
-debugSession.target.halt();
 debugSession.target.disconnect();
 debugSession.endCIOLogging();
 
