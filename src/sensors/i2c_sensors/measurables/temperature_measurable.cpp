dc0862206c1b5b9a305df7f8d7a2ce32f8522dc7
Benjamin Ding <ben@dingbenjamin.com>
2018-04-11 15:12:58 +0200

    Fix temperature sensor initialisation
    Remove faulty NULL initialisation
    
    Thank you @Akremor

--- src/sensors/i2c_sensors/measurables/temperature_measurable.cpp
+++ src/sensors/i2c_sensors/measurables/temperature_measurable.cpp
@@ -1,7 +1,7 @@
 #include <src/sensors/i2c_sensors/measurables/temperature_measurable.h>
 
 TemperatureMeasurable::TemperatureMeasurable(MCP9808* temp_sensor)
-    : I2cMeasurable<double>(NULL) {}
+    : I2cMeasurable<double>(temp_sensor) {}
 
 double TemperatureMeasurable::TakeI2cReading() {
     MCP9808* temp_sensor = static_cast<MCP9808*>(I2cMeasurable::sensor);
