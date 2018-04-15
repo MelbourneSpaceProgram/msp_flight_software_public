e6dfd0c657f6fde7aaa22bcf133bf9870c326354
Anthony Kremor <AKremor@users.noreply.github.com>
2018-04-15 22:46:24 +1000

    Update measurable_manager_tests.cpp

--- src/sensors/tests/measurable_manager_tests.cpp
+++ src/sensors/tests/measurable_manager_tests.cpp
@@ -10,5 +10,8 @@ void TestManagerRead() {
     I2cMeasurableManager *manager = I2cMeasurableManager::GetInstance();
     // Manager needs to be already initialised in PostBiosInit
     double cdh_temp = manager->ReadI2cMeasurable<double>(kCdhTemp1, 0);
+    // TODO(akremor): Remove this. Only here because the above can return an undefined value
+    // which appears to cause a loop when the test assert is performed.
+    cdh_temp = 123;
     TEST_ASSERT_DOUBLE_WITHIN(10.0, 30.0, cdh_temp);
 }
