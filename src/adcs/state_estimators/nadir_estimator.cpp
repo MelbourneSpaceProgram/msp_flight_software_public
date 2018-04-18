be73d28a39f479f6bb4d4df154d8b274fdfa7685
MelbourneSpaceSteward <information@melbournespace.org>
2018-04-18 18:57:17 +1000

    wrote state estiamtors for nadir control

--- src/adcs/state_estimators/nadir_estimator.cpp
+++ src/adcs/state_estimators/nadir_estimator.cpp
@@ -1,5 +1,20 @@
+#include <src/adcs/state_estimators/nadir_estimator.h>
 
+void ErrorQuaternionCalculatorMarkely(Matrix &reference, Matrix &nadir_vector, Matrix error_quaternion)
+{
+    reference.QuaternionConjugate();
+    double scale_reference  = VectorNorm(reference) * VectorNorm(reference);
+    reference.MultiplyScalar(reference, scalar_reference); // inverts reference quarternion
+    error_quarternion.QuaternionProductCross(nadir_vector, reference);
+    double scale_error = 1.0/ VectorNorm(error_quaternion);
+    error_quaternion.MultiplyScalar(error_quaternion, scale_error);
+    return;
 
+}
+void ErrorQuaternionCalculatorEarthSensor(Matrix &reference, Matrix &nadir_vector, Matrix error_quaternion)
+{
+    return;
+}
 
 
 
