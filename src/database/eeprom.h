1acda2a6d8f50990e59ee47b900da33bfb2703ff
MelbourneSpaceSteward <information@melbournespace.org>
2018-04-18 21:38:50 +1000

    Add Eeprom class

---
+++ src/database/eeprom.h
@@ -0,0 +1,36 @@
+#ifndef SRC_DATABASE_EEPROM_H_
+#define SRC_DATABASE_EEPROM_H_
+
+#include <Board.h>
+#include <src/util/data_types.h>
+#include <ti/sysbios/knl/Semaphore.h>
+
+enum EepromInstructions {
+    kEepromWriteStatus = 0b001,
+    kEepromWriteData = 0b010,
+    kEepromReadData = 0b011,
+    kEepromWriteDisable = 0b100,
+    kEepromReadStatus = 0b101,
+    kEepromWriteEnable = 0b110
+};
+
+class Eeprom {
+   public:
+    static void Init();
+    static bool ReadStatusRegister(byte *status_register);
+    static bool WriteStatusRegister(byte status_register);
+    static bool ReadData(uint16_t address, byte *read_buffer,
+                         uint16_t read_buffer_length, bool *valid_buffer,
+                         uint16_t valid_buffer_length);
+    static bool WriteData(uint16_t address, byte *write_buffer,
+                          uint16_t write_buffer_length);
+
+   private:
+    static const uint32_t slave_select_index = NVM_nCS;
+    static Semaphore_Handle eeprom_in_use;
+
+    static bool WriteEnable();
+    static bool WriteDisable();
+};
+
+#endif  // SRC_DATABASE_EEPROM_H_
--- src/database/tests/database_tests.cpp
+++ src/database/tests/database_tests.cpp
@@ -1,4 +1,9 @@
+#include <external/etl/exception.h>
+#include <src/config/unit_tests.h>
 #include <src/database/HammingCoder.h>
+#include <src/database/eeprom.h>
+#include <src/util/data_types.h>
+#include <test_runners/database_tests.h>
 #include <test_runners/unity.h>
 
 void TestHamming() {
@@ -33,4 +38,134 @@ void TestHamming() {
     hamming_decoded_byte = HammingCoder::Decode(hamming_encoded_byte);
     TEST_ASSERT_FALSE(hamming_decoded_byte.valid);
     TEST_ASSERT_EQUAL_UINT8(0b00000000, hamming_decoded_byte.data);
+
+    byte byte_array[2] = {0b10101010, 0b10010010};
+    byte encoded_byte_array[4], decoded_byte_array[2];
+    bool invalid_bytes[2];
+
+    bool failed = false;
+    try {
+        HammingCoder::EncodeByteArray(encoded_byte_array, 2, byte_array, 4);
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    failed = false;
+    try {
+        HammingCoder::DecodeByteArray(decoded_byte_array, 4, invalid_bytes, 4,
+                                      encoded_byte_array, 2);
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    failed = false;
+    try {
+        HammingCoder::DecodeByteArray(decoded_byte_array, 2, invalid_bytes, 3,
+                                      encoded_byte_array, 4);
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    HammingCoder::EncodeByteArray(encoded_byte_array, 4, byte_array, 2);
+    HammingCoder::DecodeByteArray(decoded_byte_array, 2, invalid_bytes, 2,
+                                  encoded_byte_array, 4);
+    TEST_ASSERT_EQUAL_UINT8(byte_array[0], decoded_byte_array[0]);
+    TEST_ASSERT_EQUAL_UINT8(byte_array[1], decoded_byte_array[1]);
+}
+
+void TestEepromReadWriteStatus(void) {
+    if (!eeprom_test_enabled) {
+        TEST_IGNORE_MESSAGE("Hardware test ignored");
+    }
+    byte status_register = 1 << 3;
+    Eeprom::WriteStatusRegister(status_register);
+    Eeprom::ReadStatusRegister(&status_register);
+
+    TEST_ASSERT_EQUAL_UINT8(24, status_register);
+}
+
+void TestEepromReadWriteData(void) {
+    if (!eeprom_test_enabled) {
+        TEST_IGNORE_MESSAGE("Hardware test ignored");
+    }
+    byte write_buffer[345];
+    uint16_t address = 1 << 10;
+    for (uint32_t i = 0; i < 345; i++) {
+        write_buffer[i] = (i * 41) % 71;
+    }
+
+    bool failed = false;
+    try {
+        Eeprom::WriteData((1 << 15), write_buffer, 345);
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    failed = false;
+    try {
+        Eeprom::WriteData(address, write_buffer, (1 << 15));
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    failed = false;
+    try {
+        Eeprom::WriteData((1 << 14), write_buffer, (1 << 14));
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    Eeprom::WriteData(address, write_buffer, 345);
+
+    byte read_buffer[345] = {0};
+    bool valid_buffer[345];
+    uint16_t offset = 17;
+    address += offset;
+
+    failed = false;
+    try {
+        Eeprom::ReadData((1 << 15), read_buffer, 345 - offset, valid_buffer,
+                         345 - offset);
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    failed = false;
+    try {
+        Eeprom::ReadData(address, read_buffer, (1 << 15), valid_buffer,
+                         (1 << 15));
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    failed = false;
+    try {
+        Eeprom::ReadData((1 << 14), read_buffer, (1 << 14), valid_buffer,
+                         (1 << 14));
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    try {
+        Eeprom::ReadData(1, read_buffer, 2, valid_buffer, 1);
+    } catch (etl::exception& e) {
+        failed = true;
+    }
+    TEST_ASSERT_TRUE(failed);
+
+    Eeprom::ReadData(address, read_buffer, 345 - offset, valid_buffer,
+                     345 - offset);
+    for (uint32_t i = 0; i < 345 - offset; i++) {
+        TEST_ASSERT_EQUAL_UINT8(write_buffer[i + offset], read_buffer[i]);
+        TEST_ASSERT_TRUE(valid_buffer[i]);
+    }
 }
