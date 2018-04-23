178055f77bee6d5ded1410c1f3cb8321b0900e30
Rowan Skewes <rowan.skewes@gmail.com>
2018-04-23 22:20:19 +1000

    Ignore IMU tests when not connected

--- src/sensors/specific_sensors/tests/magnetometer_tests.cpp
+++ src/sensors/specific_sensors/tests/magnetometer_tests.cpp
@@ -5,7 +5,7 @@
 #include <src/config/unit_tests.h>
 
 void TestMagnetometer() {
-    if (!magnetometer_test_enabled || !hil_enabled) {
+    if (!imu_test_enabled || !hil_enabled) {
         TEST_IGNORE_MESSAGE("Ignore Magnetometer tests without HIL or I2C");
     }
     Magnetometer magnetometer("blah");
