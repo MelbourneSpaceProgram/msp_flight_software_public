912e8f0667aebd849764c34f69eaf0e24b696141
Anthony Kremor <github@anthonykremor.com>
2018-04-23 10:37:42 +1000

    Allow accessing multiplexer channels by index

--- src/i2c/multiplexers/i2c_multiplexer.cpp
+++ src/i2c/multiplexers/i2c_multiplexer.cpp
@@ -18,6 +18,47 @@ void I2cMultiplexer::OpenChannel(MuxChannel channel) const {
     }
 }
 
+I2cMultiplexer::MuxChannel I2cMultiplexer::ChannelIndexToMuxChannel(
+    uint8_t channel_index) {
+    I2cMultiplexer::MuxChannel channel;
+    switch (channel_index) {
+        case 0:
+            channel = I2cMultiplexer::kMuxChannel0;
+            break;
+        case 1:
+            channel = I2cMultiplexer::kMuxChannel1;
+            break;
+        case 2:
+            channel = I2cMultiplexer::kMuxChannel2;
+            break;
+        case 3:
+            channel = I2cMultiplexer::kMuxChannel3;
+            break;
+        case 4:
+            channel = I2cMultiplexer::kMuxChannel4;
+            break;
+        case 5:
+            channel = I2cMultiplexer::kMuxChannel5;
+            break;
+        case 6:
+            channel = I2cMultiplexer::kMuxChannel6;
+            break;
+        case 7:
+            channel = I2cMultiplexer::kMuxChannel7;
+            break;
+        default:
+            Log_error0("Attempted to open invalid mux channel");
+            return I2cMultiplexer::kMuxNoChannel;
+    }
+
+    return channel;
+}
+
+void I2cMultiplexer::OpenChannel(uint8_t channel_index) const {
+    MuxChannel channel = ChannelIndexToMuxChannel(channel_index);
+    this->OpenChannel(channel);
+}
+
 void I2cMultiplexer::CloseChannel(MuxChannel channel) const {
     if (channel == kMuxNoChannel) {
         Log_error0("Attempted to close invalid mux channel");
@@ -26,6 +67,11 @@ void I2cMultiplexer::CloseChannel(MuxChannel channel) const {
         write_buffer &= ~(1 << static_cast<uint8_t>(channel));
         GetBus()->PerformWriteTransaction(GetAddress(), &write_buffer, 1);
     }
+}
+
+void I2cMultiplexer::CloseChannel(uint8_t channel_index) const {
+    MuxChannel channel = ChannelIndexToMuxChannel(channel_index);
+    this->CloseChannel(channel);
 }
 
 void I2cMultiplexer::CloseAllChannels() const {
