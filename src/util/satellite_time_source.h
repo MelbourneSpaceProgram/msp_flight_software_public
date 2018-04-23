8713d1608c8b5c5e6e5186bbcb02469a01253e4a
Anthony Kremor <github@anthonykremor.com>
2018-04-23 11:28:38 +1000

    Iterate on feedback.
    Use nanopb Time message.
    Use SleepMilli.
    Improve log error message when time conversion was not performed.
    Remove logging from noisy functions.

--- src/util/satellite_time_source.h
+++ src/util/satellite_time_source.h
@@ -1,21 +1,16 @@
 #ifndef SRC_UTIL_SATELLITE_TIME_SOURCE_H_
 #define SRC_UTIL_SATELLITE_TIME_SOURCE_H_
 
+#include <src/messages/Time.pb.h>
 #include <src/util/data_types.h>
-
-typedef struct SatelliteTime_t {
-    bool valid;
-    uint32_t epoch_seconds;
-    uint16_t milliseconds;
-} SatelliteTime;
 
 class SatelliteTimeSource {
    public:
     static void SetTime(RTime time);
-    static SatelliteTime GetTime();
+    static Time GetTime();
 
    private:
-    static SatelliteTime satellite_time;
+    static Time satellite_time;
     static const uint16_t kSatelliteEpochYear = 1970;
 };
 
