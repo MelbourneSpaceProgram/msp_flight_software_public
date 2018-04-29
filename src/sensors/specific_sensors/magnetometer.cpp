dbf321a0f4cc281f5e63c88d023f67a95c4a33ca
Rowan Skewes <rowan.skewes@gmail.com>
2018-04-29 15:09:46 +1000

    Poll magnetometer before orientation control starts up
    Orientation control will start after the antenna is deployed,
    and this task is to collect magnetometer data from ejection to
    antenna deployment.
    
    When the orientation control task starts, this task will exit and
    the orientation control task will use the readings stored in the
    buffer to calibrate the magnetometer.

--- src/sensors/specific_sensors/magnetometer.cpp
+++ src/sensors/specific_sensors/magnetometer.cpp
@@ -13,24 +13,44 @@
 #include <ti/sysbios/BIOS.h>
 #include <ti/sysbios/knl/Semaphore.h>
 
-Magnetometer::Magnetometer(std::string id) : id(id) {}
-
+// Get readings from the hardware magnetometer and the simulation.
+// Use the simulation readings for the controller, but echo the hardware
+// readings to the DebugClient to show that it's working.
+//////////////////////////////////////////////////
+// For hardware tests, make sure to store the hardware readings
+// for calibration.
+// For HIL tests, store the simulation readings for calibration.
 bool Magnetometer::TakeReading() {
+    // TODO(rskew): Implement the I2C read
+
     if (hil_enabled) {
-        return TakeReadingHil();
+        // Echo reading to data dashboard
+        RunnableDataDashboard::TransmitMessage(
+            kMagnetometerReadingCode, MagnetometerReading_size,
+            MagnetometerReading_fields, &reading);
     }
 
-    // TODO(rskew): Implement the I2C read
-    return false;
+    // try {
+    //     calibration_readings_buffer.WriteMessage(reading);
+    // } catch (etl::exception e) {
+    //     catch exceptions for pb encode error, sdcard write error,
+    // }
+
+    // TODO (rskew) combine noise from hardware readings with simulated readings
+    bool success;
+    if (hil_enabled) {
+        success = TakeReadingHil();
+    }
+
+    return success;
 }
 
 bool Magnetometer::TakeReadingHil() {
     DebugStream *debug_stream = DebugStream::GetInstance();
     uint8_t buffer[MagnetometerReading_size];
 
-    bool success =
-        debug_stream->RequestMessageFromSimulator(kMagnetometerReadingRequestCode,
-                                                  buffer, MagnetometerReading_size);
+    bool success = debug_stream->RequestMessageFromSimulator(
+        kMagnetometerReadingRequestCode, buffer, MagnetometerReading_size);
 
     if (success) {
         pb_istream_t stream =
@@ -38,8 +58,7 @@ bool Magnetometer::TakeReadingHil() {
 
         bool status = pb_decode(&stream, MagnetometerReading_fields, &reading);
         if (!status) {
-            etl::exception e("pb_decode failed",
-                             __FILE__, __LINE__);
+            etl::exception e("pb_decode failed", __FILE__, __LINE__);
             throw e;
         }
 
