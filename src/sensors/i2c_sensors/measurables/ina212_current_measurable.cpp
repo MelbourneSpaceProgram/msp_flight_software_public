a6af235651ffd726237bf65472e3d7a3f7bd5d81
Will Schuetz <willschuetz@outlook.com>
2018-05-19 10:20:25 +1000

    Adds remaining current sensors Add measurables fix include guard Add unit tests

---
+++ src/sensors/i2c_sensors/measurables/ina212_current_measurable.cpp
@@ -0,0 +1,9 @@
+#include <src/sensors/i2c_sensors/measurables/ina212_current_measurable.h>
+
+Ina212CurrentMeasurable::Ina212CurrentMeasurable(Ina212* current_sensor)
+    : I2cMeasurable<double>(current_sensor) {}
+
+double Ina212CurrentMeasurable::TakeI2cReading() {
+    Ina212* current_sensor = static_cast<Ina212*>(I2cMeasurable::sensor);
+    return current_sensor->TakeCurrentReading();
+}
