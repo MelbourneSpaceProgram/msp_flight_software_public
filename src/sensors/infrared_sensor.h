8d4334c2131d1f08515d5907d6c464a481d7f526
MelbourneSpaceSteward <information@melbournespace.org>
2018-05-28 19:53:58 +1000

    Moved constant values into class files from headers

--- src/sensors/infrared_sensor.h
+++ src/sensors/infrared_sensor.h
@@ -3,12 +3,6 @@
 
 #include <src/util/matrix.h>
 
-const double sensor_a_poly_coeffs[10] = {
-    -2.362272825932358e+03, 1.164697776531066e+04,  -2.457026272978545e+04,
-    2.887425236691933e+04,  -2.061119154675368e+04, 9.135824661573712e+03,
-    -2.464410337515545e+03, 3.785871673586688e+02,  -30.453780230032056,
-    2.973281695942479};
-
 class InfraredSensor {
    public:
     explicit InfraredSensor(Matrix &side_normal_matrix);
