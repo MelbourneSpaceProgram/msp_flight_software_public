a55b305c0dd95a73a7e361d1514ada636068f18e
Anthony Kremor <github@anthonykremor.com>
2018-06-01 13:01:29 +1000

    Format and remove unused variables in watchdog

--- src/util/system_watchdog.cpp
+++ src/util/system_watchdog.cpp
@@ -2,24 +2,21 @@
 #include <xdc/runtime/Log.h>
 
 Watchdog_Handle SystemWatchdog::watchdog_handle = NULL;
-uint32_t SystemWatchdog::kTimeoutLength = 0xFFFFFFFF;
 
-SystemWatchdog::SystemWatchdog(uint16_t index, uint32_t timeout){
+SystemWatchdog::SystemWatchdog(uint16_t index) {
     Watchdog_init();
     Watchdog_Params params;
-
-    kTimeoutLength = timeout;
 
     Watchdog_Params_init(&params);
     watchdog_handle = Watchdog_open(index, &params);
 
-    if(!watchdog_handle){
+    if (!watchdog_handle) {
         Log_error0("Watchdog failed to open");
     }
 }
 
-void SystemWatchdog::ResetTimer(){
-    if (!watchdog_handle){
+void SystemWatchdog::ResetTimer() {
+    if (!watchdog_handle) {
         Log_error0("Calling reset on uninitialised watchdog");
         return;
     }
