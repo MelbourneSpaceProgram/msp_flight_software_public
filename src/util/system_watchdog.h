7273dd689c0bb9f92e35721c1924334608902506
Anthony Kremor <github@anthonykremor.com>
2018-06-01 13:01:29 +1000

    Format and remove unused variables in watchdog

--- src/util/system_watchdog.h
+++ src/util/system_watchdog.h
@@ -1,19 +1,16 @@
 #ifndef SRC_UTIL_SYSTEM_WATCHDOG_H_
 #define SRC_UTIL_SYSTEM_WATCHDOG_H_
 
-
 #include <ti/drivers/Watchdog.h>
 #include <ti/drivers/watchdog/WatchdogMSP432.h>
 
-class SystemWatchdog{
-  public:
-    SystemWatchdog(uint16_t index, uint32_t timeout);
+class SystemWatchdog {
+   public:
+    SystemWatchdog(uint16_t index);
     static void ResetTimer();
-  private:
-    static uint32_t kTimeoutLength;
-    static Watchdog_Handle watchdog_handle;
 
-    static void SetTimeoutLength(uint32_t timeout);
+   private:
+    static Watchdog_Handle watchdog_handle;
 };
 
-#endif //SRC_UTIL_SYSTEM_WATCHDOG_H_
+#endif  // SRC_UTIL_SYSTEM_WATCHDOG_H_
