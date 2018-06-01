056174d841387a4ad28782b52c8a6e990ad46b59
Anthony Kremor <github@anthonykremor.com>
2018-06-01 13:03:55 +1000

    Remove unused includes

--- src/init/init.h
+++ src/init/init.h
@@ -1,15 +1,6 @@
 #ifndef SRC_INIT_INIT_H_
 #define SRC_INIT_INIT_H_
 
-#include <ti/sysbios/knl/Task.h>
-
-/* TI-RTOS Header files */
-#include <ti/drivers/GPIO.h>
-#include <ti/drivers/I2C.h>
-#include <ti/drivers/SPI.h>
-#include <ti/drivers/UART.h>
-#include <ti/drivers/Watchdog.h>
-
 void PreBiosInit();
 void PostBiosInit();
 void TestInit();
