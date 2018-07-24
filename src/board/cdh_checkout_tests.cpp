f492fa590726bf8e1cdd31ce92ac01c8dd3a7e48
Anthony Kremor <github@anthonykremor.com>
2018-07-24 23:12:01 +1000

    Create CDH measurable tests

---
+++ src/board/cdh_checkout_tests.cpp
@@ -0,0 +1,13 @@
+#include <CppUTest/TestHarness.h>
+#include <src/config/unit_tests.h>
+#include <src/sensors/i2c_measurable_manager.h>
+
+TEST_GROUP(CdhCheckout){};
+
+static I2cMeasurableManager* i2c_measurable_manager = I2cMeasurableManager::GetInstance();
+
+TEST(CdhCheckout, CanSenseSysTemperature) {
+
+    double cdh_sys_temp = i2c_measurable_manager->ReadI2cMeasurable<double>(kCdhSysTemp, 0);
+    DOUBLES_EQUAL(20, cdh_sys_temp, 5);
+}
