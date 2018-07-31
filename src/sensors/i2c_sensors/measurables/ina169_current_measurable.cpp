071259fe5c0d07d816a8a354d5fec89101af6222
Benjamin Ding <ben@dingbenjamin.com>
2018-07-31 22:25:22 +1000

    Remove deprecated current sensor measurables

--- src/sensors/i2c_sensors/measurables/ina169_current_measurable.cpp
+++
@@ -1,9 +0,0 @@
-#include <src/sensors/i2c_sensors/measurables/ina169_current_measurable.h>
-
-Ina169CurrentMeasurable::Ina169CurrentMeasurable(Ina169* current_sensor)
-    : I2cMeasurable<double>(current_sensor, kFailedCurrentReading) {}
-
-double Ina169CurrentMeasurable::TakeDirectI2cReading() {
-    Ina169* current_sensor = static_cast<Ina169*>(I2cMeasurable::sensor);
-    return current_sensor->TakeCurrentReading();
-}
--- src/sensors/i2c_sensors/measurables/ina212_current_measurable.cpp
+++
@@ -1,9 +0,0 @@
-#include <src/sensors/i2c_sensors/measurables/ina212_current_measurable.h>
-
-Ina212CurrentMeasurable::Ina212CurrentMeasurable(Ina212* current_sensor)
-    : I2cMeasurable<double>(current_sensor, kFailedCurrentReading) {}
-
-double Ina212CurrentMeasurable::TakeDirectI2cReading() {
-    Ina212* current_sensor = static_cast<Ina212*>(I2cMeasurable::sensor);
-    return current_sensor->TakeCurrentReading();
-}
