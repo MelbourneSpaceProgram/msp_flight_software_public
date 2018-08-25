9d839493ffafea9319a968e77d60a99191a24d40
Benjamin Ding <ben@dingbenjamin.com>
2018-08-25 21:18:51 +1000

    Clean beacon and implement arbitrary scaling
    Fixes
    
    
    Fix

--- src/telecomms/tests/beacon_payload_tests.cpp
+++ src/telecomms/tests/beacon_payload_tests.cpp
@@ -3,113 +3,35 @@
 
 TEST_GROUP(BeaconPayload){};
 
-TEST(BeaconPayload, TestCurrentData) {
-    BeaconPayload test_payload = new BeaconPayload();
-
-    float test_current = BeaconPayload::kCurrentUpperBound/2;
+TEST(BeaconPayload, TestConstrain) {
+    float test_current = BeaconPayload::kCurrentUpperBound / 2;
     float test_ub = BeaconPayload::kCurrentUpperBound;
     float test_lb = -BeaconPayload::kCurrentUpperBound;
-    float test_greater_ub = BeaconPayload::kCurrentUpperBound*2;
-    float test_below_lb = -BeaconPayload::kCurrentUpperBound*2;
+    float test_greater_ub = BeaconPayload::kCurrentUpperBound * 2;
+    float test_below_lb = -BeaconPayload::kCurrentUpperBound * 2;
 
-    test_payload.SetCommsRegulator1OutputCurrent(test_current);
-
-    CHECK_EQUAL(test_payload.CommsRegulator1OutputCurrent(),
-                test_current);
-
-    test_payload.SetCommsRegulator1OutputCurrent(test_ub);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputCurrent(),
-                BeaconPayload::kCurrentUpperBound);
-
-    test_payload.SetCommsRegulator1OutputCurrent(test_lb);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputCurrent(),
-                -BeaconPayload::kCurrentUpperBound);
-
-    test_payload.SetCommsRegulator1OutputCurrent(test_greater_lb);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputCurrent(),
-                BeaconPayload::kCurrentUpperBound);
-
-    test_payload.SetCommsRegulator1OutputCurrent(test_below_lb);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputCurrent(),
-                -BeaconPayload::kCurrentUpperBound);
-
-    free(test_payload);
+    CHECK_EQUAL(test_current,
+                BeaconPayload::ConstrainToRange(
+                    test_current, BeaconPayload::kCurrentUpperBound));
+    CHECK_EQUAL(BeaconPayload::kCurrentUpperBound,
+                BeaconPayload::ConstrainToRange(
+                    test_ub, BeaconPayload::kCurrentUpperBound));
+    CHECK_EQUAL(-BeaconPayload::kCurrentUpperBound,
+                BeaconPayload::ConstrainToRange(
+                    test_lb, BeaconPayload::kCurrentUpperBound));
+    CHECK_EQUAL(BeaconPayload::kCurrentUpperBound,
+                BeaconPayload::ConstrainToRange(
+                    test_greater_ub, BeaconPayload::kCurrentUpperBound));
+    CHECK_EQUAL(-BeaconPayload::kCurrentUpperBound,
+                BeaconPayload::ConstrainToRange(
+                    test_below_lb, BeaconPayload::kCurrentUpperBound));
 }
 
-TEST(BeaconPayload, TestVoltageData) {
-    BeaconPayload test_payload = new BeaconPayload();
-
-    float test_voltage = BeaconPayload::kVoltageUpperBound/2;
-    float test_ub = BeaconPayload::kVoltageUpperBound;
-    float test_lb = -BeaconPayload::kVoltageUpperBound/2;
-    float test_greater_ub = BeaconPayload::kVoltageUpperBound*2;
-    float test_below_lb = -BeaconPayload::kVoltageUpperBound*2;
-
-    test_payload.SetCommsRegulator1OutputVoltage(test_voltage);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputVoltage(),
-                test_voltage);
-
-    test_payload.SetCommsRegulator1OutputVoltage(test_ub);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputVoltage(),
-                BeaconPayload::kVoltageUpperBound);
-
-    test_payload.SetCommsRegulator1OutputVoltage(test_lb);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputVoltage(),
-                -BeaconPayload::kVoltageUpperBound);
-
-    test_payload.SetCommsRegulator1OutputVoltage(test_greater_lb);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputVoltage(),
-                BeaconPayload::kVoltageUpperBound);
-
-    test_payload.SetCommsRegulator1OutputVoltage(test_below_lb);
-
-    CHECK_EQUAL(test_payload.GetCommsRegulator1OutputVoltage(),
-                -BeaconPayload::kVoltageUpperBound);
-
-    free(test_payload);
-}
-
-TEST(BeaconPayload, TestTempData) {
-  BeaconPayload test_payload = new BeaconPayload();
-
-  byte test_temp = BeaconPayload::kTempUpperBound/2;
-  byte test_ub = BeaconPayload::kTempUpperBound;
-  byte test_lb = -BeaconPayload::kTempUpperBound;
-  byte test_greater_ub = BeaconPayload::kTempUpperBound*2;
-  byte test_below_lb = -BeaconPayload::kTempUpperBound*2;
-
-  test_payload.SetCommsTemp2(test_temp);
-
-  CHECK_EQUAL(test_payload.GetCommsTemp2(),
-              test_temp);
-
-  test_payload.SetCommsTemp2(test_ub);
-
-  CHECK_EQUAL(test_payload.GetCommsTemp2(),
-              BeaconPayload::kTempUpperBound);
-
-  test_payload.SetCommsTemp2(test_lb);
-
-  CHECK_EQUAL(test_payload.GetCommsTemp2(),
-              -BeaconPayload::kTempUpperBound);
-
-  test_payload.SetCommsTemp2(test_greater_lb);
-
-  CHECK_EQUAL(test_payload.GetCommsTemp2(),
-              BeaconPayload::kTempUpperBound);
-
-  test_payload.SetCommsTemp2(test_below_lb);
-
-  CHECK_EQUAL(test_payload.GetCommsTemp2(),
-              -BeaconPayload::kTempUpperBound);
-
-  free(test_payload);
+TEST(BeaconPayload, TestScaling) {
+    CHECK_EQUAL(1000, BeaconPayload::ScaleArbitraryInt16(500, 16384));
+    CHECK_EQUAL(32767, BeaconPayload::ScaleArbitraryInt16(1213, 1213));
+    CHECK_EQUAL(-32768, BeaconPayload::ScaleArbitraryInt16(-1213, 1213));
+    CHECK_EQUAL(0, BeaconPayload::ScaleArbitraryInt16(0, 1213))
+    CHECK_EQUAL(993, BeaconPayload::ScaleArbitraryInt16(231, 7623));
+    CHECK_EQUAL(-993, BeaconPayload::ScaleArbitraryInt16(-231, 7623));
 }
