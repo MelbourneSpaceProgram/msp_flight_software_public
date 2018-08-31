833e105237e701ad4c387cef47458a4f94bb2300
Benjamin Ding <ben@dingbenjamin.com>
2018-08-31 22:36:31 +1000

    Allow method chaining in RebuildableMessageFieldIterator
    Throw exceptions on failure

--- src/messages/rebuildable_message_field_iterator.h
+++ src/messages/rebuildable_message_field_iterator.h
@@ -1,6 +1,7 @@
 #ifndef SRC_MESSAGES_REBUILDABLE_MESSAGE_FIELD_ITERATOR_H_
 #define SRC_MESSAGES_REBUILDABLE_MESSAGE_FIELD_ITERATOR_H_
 
+#include <external/etl/exception.h>
 #include <src/messages/message.h>
 #include <src/util/data_types.h>
 #include <src/util/message_codes.h>
@@ -12,18 +13,22 @@ class RebuildableMessageFieldIterator {
         : serialised_message_buffer(serial_buffer),
           serialised_length(serialised_length) {}
     template <class T>
-    void FetchData(T* data) {
+    RebuildableMessageFieldIterator* FetchData(T* data) {
         // The ordering of calls must be the reverse of AddData
 
         if (sizeof(T) > serialised_length) {
             // Attempting to read more bytes than are present in the message
-            // TODO(akremor): Error handler
-            return;
+            etl::exception e(
+                "Rebuildable message iterator buffer size overflow", __FILE__,
+                __LINE__);
+            throw e;
         }
 
         serialised_length -= sizeof(T);
-        memcpy(data, serialised_message_buffer + serialised_length,
-                    sizeof(T));
+        // TODO(dingbenjamin): Handle copying memory into invalid type
+        memcpy(data, serialised_message_buffer + serialised_length, sizeof(T));
+
+        return this;
     }
 
    private:
