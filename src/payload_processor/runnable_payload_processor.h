432b96099fe3cc5aef4c5b2db3cc282349bf5864
Daniel Sutherland <danielmail40@gmail.com>
2018-09-15 12:12:23 +1000

    Hmac authentication implemented

--- src/payload_processor/runnable_payload_processor.h
+++ src/payload_processor/runnable_payload_processor.h
@@ -7,6 +7,7 @@ class RunnablePayloadProcessor : public Runnable {
    public:
     fnptr GetRunnablePointer();
     RunnablePayloadProcessor();
+    static const uint8_t kMspSignatureBytes = 4;
 
    private:
     static void ExecuteCommandsInLithiumPayload();
@@ -14,6 +15,8 @@ class RunnablePayloadProcessor : public Runnable {
     static const uint8_t kSignatureBytes = 4;
     static const uint8_t kLengthBytes = 1;
     static const uint8_t kSequenceNumberBytes = 2;
+    static const uint8_t kNumBytesTruncated = 4;
+    static const uint8_t kNumCharsPerByte = 2;  // (byte in hex representation)
 };
 
 #endif  //  SRC_PAYLOAD_PROCESSOR_RUNNABLE_PAYLOAD_PROCESSOR_H_
---
+++ src/util/string_hex.h
@@ -0,0 +1,11 @@
+#ifndef SRC_UTIL_STRING_HEX_H_
+#define SRC_UTIL_STRING_HEX_H_
+
+#include <string>
+
+class StringHex {
+   public:
+    static std::string StringToHex(const std::string& input);
+};
+
+#endif  // SRC_UTIL_STRING_HEX_H_
