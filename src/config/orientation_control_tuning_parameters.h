71a022a4d6645038ee291dbf730660d3c579d0d0
MelbourneSpaceSteward <information@melbournespace.org>
2018-10-04 21:41:31 +1000

    fixed control params

--- src/config/orientation_control_tuning_parameters.h
+++ src/config/orientation_control_tuning_parameters.h
@@ -2,4 +2,4 @@
 
 const uint16_t kBDotEstimatorTimeConstantMillis = 4000;
 
-const double kBDotControllerGains[3] = {60000.0, 60000.0, 60000.0};
+const double kBDotControllerGains[3] = {30000.0, 30000.0, 30000.0};
