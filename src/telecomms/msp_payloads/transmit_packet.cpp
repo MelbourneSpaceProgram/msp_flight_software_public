0621c7f3735b030461bd3f6658c6091c1e3fa08b
MelbourneSpaceSteward <information@melbournespace.org>
2018-10-07 22:14:28 +1100

    unstashed changes

--- src/telecomms/msp_payloads/transmit_packet.cpp
+++ src/telecomms/msp_payloads/transmit_packet.cpp
@@ -2,6 +2,7 @@
 #include <src/telecomms/lithium.h>
 #include <src/telecomms/msp_payloads/transmit_packet.h>
 #include <src/telecomms/msp_payloads/transmit_payload.h>
+#include <src/telecomms/fec/rs8.h>
 
 TransmitPacket::TransmitPacket(TransmitPayload *transmit_payload)
     : tx_count(Lithium::GetTxCounter()),
@@ -17,6 +18,12 @@ SerialisedMessage TransmitPacket::SerialiseTo(byte *serial_buffer) const {
         .AddData<uint8_t>(command_success_count)
         .AddData<uint8_t>(transmit_payload->GetPayloadCode())
         .AddMessage(transmit_payload);
+
+    byte payload_parity[Rs8::kParityLength] = {0};
+    byte payload_fec[Rs8::kBlockLength] = {0};
+    Rs8::Encode(serial_buffer, payload_parity);
+    memcpy(payload_fec, payload, Rs8::kDataLength);
+    memcpy(&payload_fec[Rs8::kDataLength], payload_parity, Rs8::kParityLength);
 
     return builder.Build();
 }
