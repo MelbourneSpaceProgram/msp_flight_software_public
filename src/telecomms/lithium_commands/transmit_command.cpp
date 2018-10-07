5a6c8d1279ebbdc7c04f7b698a6c0c54037a46c7
MelbourneSpaceSteward <information@melbournespace.org>
2018-10-07 22:12:41 +1100

    unstashed changes

--- src/telecomms/lithium_commands/transmit_command.cpp
+++ src/telecomms/lithium_commands/transmit_command.cpp
@@ -1,20 +1,10 @@
 #include <src/telecomms/lithium.h>
 #include <src/telecomms/lithium_commands/lithium_command_codes.h>
 #include <src/telecomms/lithium_commands/transmit_command.h>
-#include <src/telecomms/fec/rs8.h>
 
 TransmitCommand::TransmitCommand(TransmitPayload *transmit_payload)
     : transmit_packet(transmit_payload),
       LithiumCommand(kTransmitCommandCode, &transmit_packet) {}
-
-byte *TransmitCommand::DoFec(byte *payload) {
-	byte payload_parity[Rs8::kParityLength] = {0};
-	byte payload_fec[Rs8::kBlockLength] = {0};
-	Rs8::Encode(payload, payload_parity);
-	memcpy(payload_fec, payload, Rs8::kDataLength);
-	memcpy(&payload_fec[Rs8::kDataLength], payload_parity, Rs8::kParityLength);
-	// Do stuff with payload_fec...
-}
 
 uint16_t TransmitCommand::GetLithiumPayloadSize() const {
     return transmit_packet.GetSerialisedSize();
