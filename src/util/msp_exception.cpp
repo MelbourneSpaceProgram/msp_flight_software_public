e2a2246a0a239561943d1d9b85baff3dee0026f2
Benjamin Ding <ben@dingbenjamin.com>
2018-10-26 02:17:14 +1100

    Implement MSP exception type
    Includes global static MSP exception circular buffer
    Exceptions are timestamped
    Backwards compatible with usage of etl::exception

--- src/util/msp_exception.cpp
+++ src/util/msp_exception.cpp
@@ -1,5 +1,27 @@
 #include <src/util/msp_exception.h>
+#include <src/util/satellite_time_source.h>
 #include <string>
+
+etl::deque<SerialisedException, MspException::kNumExceptionsInBuffer>
+    MspException::deque;
+
+MspException::MspException(string_type reason, uint8_t task_id,
+                           uint8_t subsystem_id, uint8_t error_id,
+                           uint8_t stack_level, string_type file,
+                           numeric_type line)
+    : exception(reason, file, line),
+      serialised_exception({task_id, subsystem_id, error_id, stack_level,
+                            SatelliteTimeSource::GetTime().timestamp_ms}) {}
+
+void MspException::LogException(const MspException& e) {
+    // Circular buffer
+    if (deque.full()) deque.pop_front();
+    deque.push_back(e.GetSerialisedException());
+
+    Log_error3("Exception Occurred: %s\n File: %s, Line %d",
+               CharToIarg(e.what()), CharToIarg(e.file_name()),
+               e.line_number());
+}
 
 void MspException::LogException(etl::exception& e) {
     Log_error3("Exception Occurred: %s\n File: %s, Line %d",
@@ -7,7 +29,51 @@ void MspException::LogException(etl::exception& e) {
                e.line_number());
 }
 
+SerialisedException MspException::PopException() {
+    // Invalid exception
+    if (deque.empty())
+        return {kInvalidExceptionId, kInvalidExceptionId, kInvalidExceptionId,
+                kInvalidExceptionId};
+
+    SerialisedException popped_exception = deque.front();
+    deque.pop_front();
+    return popped_exception;
+}
+
+SerialisedException MspException::PeekException() { return deque.front(); }
+
+void MspException::ClearExceptions() { deque.clear(); }
+
+uint16_t MspException::GetNumExceptions() {
+    // TODO(dingbenjamin): Check if this is in bytes or number of exceptions
+    return deque.size();
+}
+
+bool MspException::IsExceptionsFull() { return deque.full(); }
+
 xdc_IArg MspException::CharToIarg(const char* string) {
     int iarg = reinterpret_cast<unsigned int>(string);
     return iarg;
+}
+
+const SerialisedException& MspException::GetSerialisedException() const {
+    return serialised_exception;
+}
+
+uint8_t MspException::GetTaskId() const { return serialised_exception.task_id; }
+
+uint8_t MspException::GetSubsystemId() const {
+    return serialised_exception.subsystem_id;
+}
+
+uint8_t MspException::GetErrorId() const {
+    return serialised_exception.error_id;
+}
+
+uint8_t MspException::GetStackLevel() const {
+    return serialised_exception.stack_level;
+}
+
+uint64_t MspException::GetTimestamp() const {
+    return serialised_exception.timestamp_ms;
 }
--- src/util/msp_exception.h
+++ src/util/msp_exception.h
@@ -1,15 +1,67 @@
-#ifndef SRC_UTILS_ETL_UTILS_H_
-#define SRC_UTILS_ETL_UTILS_H_
+#ifndef SRC_UTILS_MSP_EXCEPTION_H_
+#define SRC_UTILS_MSP_EXCEPTION_H_
 
+#include <external/etl/deque.h>
 #include <external/etl/exception.h>
 #include <xdc/runtime/Log.h>
 
-class MspException {
+#define QuoteMacro(string) "\"" string "\""
+#define QuoteHyphenateMacro(a, b, c, d) Quote(#a "-" #b "-" #c "-" #d)
+#define MspExceptionMacro(reason, task_id, subsystem_id, error_id,          \
+                          stack_level)                                      \
+    MspException(                                                           \
+        ETL_ERROR_TEXT(reason, QuoteHyphenateMacro(task_id, subsystem_id,   \
+                                                   error_id, stack_level)), \
+        task_id, subsystem_id, error_id, stack_level, __FILE__, __LINE__)
+
+// Usage:
+// throw MspExceptionMacro(verbose_reason, task_id, subsystem_id, error_id,
+// stack_level);
+//
+// task_id, subsystem_id, error_id are all intended to help identify the problem
+// on the ground
+//
+// TODO(dingbenjamin): stack_level gives an indication of how many levels the
+// exception has propagated
+
+// Should fit into a single 32 bit word for nice alignment
+typedef struct SerialisedException {
+    uint8_t task_id;  // TODO(dingbenjamin): Retrieve this at runtime
+    uint8_t subsystem_id;
+    uint8_t error_id;
+    uint8_t stack_level;
+    uint64_t timestamp_ms;
+} SerialisedException;
+
+class MspException : public etl::exception {
    public:
+    static void LogException(const MspException& e);
+    // TODO(dingbenjamin): Remove once all uses of etl::exception removed
     static void LogException(etl::exception& e);
+    static SerialisedException PeekException();
+    static SerialisedException PopException();
+    static void ClearExceptions();
+    static uint16_t GetNumExceptions();
+    static bool IsExceptionsFull();
+
+    MspException(string_type reason, uint8_t task_id, uint8_t subsystem_id,
+                 uint8_t error_id, uint8_t stack_level, string_type file,
+                 numeric_type line);
+    const SerialisedException& GetSerialisedException() const;
+    uint8_t GetTaskId() const;
+    uint8_t GetSubsystemId() const;
+    uint8_t GetErrorId() const;
+    uint8_t GetStackLevel() const;
+    uint64_t GetTimestamp() const;
 
    private:
+    static void PushException(const SerialisedException& serial_exception);
     static xdc_IArg CharToIarg(const char* string);
+    static constexpr uint16_t kNumExceptionsInBuffer = 500;
+    static constexpr uint8_t kInvalidExceptionId = 255;
+    static etl::deque<SerialisedException, kNumExceptionsInBuffer> deque;
+
+    const SerialisedException serialised_exception;
 };
 
-#endif  // SRC_UTILS_ETL_UTILS_H_
+#endif  // SRC_UTILS_MSP_EXCEPTION_H_
