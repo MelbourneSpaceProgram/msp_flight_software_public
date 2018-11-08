070887125100817b53cfee781182cf4f10954cc0
HUGO-SURFACE\Hugo <hhumbers@gmail.com>
2018-11-09 01:54:43 +1100

    Move code around to make it compile

--- src/database/flash_memory/flash_storables/flash_storable_tle.cpp
+++ src/database/flash_memory/flash_storables/flash_storable_tle.cpp
@@ -1,12 +1,17 @@
 #include <src/database/flash_memory/flash_storables/flash_storable_tle.h>
 
 FlashStorableTle::FlashStorableTle(Tle& tle)
-    : tle_data.mean_motion(tle.mean_motion),
-    tle_data.mean_anomaly(tle.mean_anomaly), tle_data.inclination(
-                                                 tle.inclination),
-    tle_data.raan(tle.raan), tle_data.bstar_drag(tle.bstar_drag),
-    tle_data.epoch(tle.epoch), tle_data.eccentricity_1e7(tle.eccentricity_1e7),
-    tle_data.argument_of_perigee(tle.argument_of_perigee) {}
+    : FlashStorable<FlashStorableTleStruct>(
+          kFlashStorableTleFlashStorageAddress) {
+    tle_data.mean_motion = tle.mean_motion;
+    tle_data.mean_anomaly = tle.mean_anomaly;
+    tle_data.inclination = tle.inclination;
+    tle_data.raan = tle.raan;
+    tle_data.bstar_drag = tle.bstar_drag;
+    tle_data.epoch = tle.epoch;
+    tle_data.eccentricity_1e7 = tle.eccentricity_1e7;
+    tle_data.argument_of_perigee = tle.argument_of_perigee;
+}
 
 void FlashStorableTle::ConvertToFlashStorableStruct(
     FlashStorableTleStruct* flash_storable_tle_struct_ptr) {
@@ -21,7 +26,7 @@ void FlashStorableTle::ConvertToFlashStorableStruct(
 }
 
 void FlashStorableTle::UpdateFromFlashStorableStruct(
-    FlashStorableTleStruct* flash_storable_tle_struct) {
+    FlashStorableTleStruct* flash_storable_tle_struct_ptr) {
     tle_data.mean_motion = flash_storable_tle_struct_ptr->mean_motion;
     tle_data.mean_anomaly = flash_storable_tle_struct_ptr->mean_anomaly;
     tle_data.inclination = flash_storable_tle_struct_ptr->inclination;
--- src/database/flash_memory/flash_storables/flash_storable_tle.h
+++ src/database/flash_memory/flash_storables/flash_storable_tle.h
@@ -1,12 +1,13 @@
 #ifndef SRC_DATABASE_FLASH_MEMORY_FLASH_STORABLES_FLASH_STORABLE_TLE_H_
 #define SRC_DATABASE_FLASH_MEMORY_FLASH_STORABLES_FLASH_STORABLE_TLE_H_
 
+#include <src/database/flash_memory/flash_storables/flash_storable.h>
 #include <src/database/flash_memory/flash_storables/flash_storable_data_types.h>
 #include <src/messages/Tle.pb.h>
 
-class FlashStorableTle : FlashStorable<FlashStorableTleStruct> {
+class FlashStorableTle : public FlashStorable<FlashStorableTleStruct> {
    public:
-    FlashStorableTle(Tle &tle);
+    FlashStorableTle(Tle& tle);
 
    private:
     void ConvertToFlashStorableStruct(
