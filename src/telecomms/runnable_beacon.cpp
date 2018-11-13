c04b4525b64b24ed117a68143dbc63ed53fbf4c2
Rowan Skewes <rowan.skewes@gmail.com>
2018-11-13 23:45:32 +1100

    Cut power to flight systems when beaconing

--- src/telecomms/runnable_beacon.cpp
+++ src/telecomms/runnable_beacon.cpp
@@ -3,6 +3,8 @@
 #include <src/telecomms/lithium_commands/transmit_command.h>
 #include <src/telecomms/msp_payloads/beacon_payload.h>
 #include <src/telecomms/runnable_beacon.h>
+#include <src/util/msp_exception.h>
+#include <src/util/satellite_power.h>
 #include <src/util/task_utils.h>
 #include <xdc/runtime/Log.h>
 
@@ -13,7 +15,7 @@ RunnableBeacon::RunnableBeacon() {}
 fnptr RunnableBeacon::GetRunnablePointer() { return &Beacon; }
 
 void RunnableBeacon::Beacon() {
-    Lithium *lithium = Lithium::GetInstance();
+    Lithium* lithium = Lithium::GetInstance();
 
     while (1) {
         // TODO(dingbenjamin): Implement remaining beacon fields
@@ -21,10 +23,20 @@ void RunnableBeacon::Beacon() {
         // Avoid building the packet if transmit is disabled
         if (lithium->IsTransmitEnabled()) {
             BeaconPayload beacon;
+            try {
+                SatellitePower::CutPowerToFlightSystems();
+            } catch (etl::exception& e) {
+                MspException::LogException(e);
+            }
             if (lithium->Transmit(&beacon)) {
                 Log_info0("Beacon transmission succeeded");
             } else {
                 Log_error0("Beacon transmission failed");
+            }
+            try {
+                SatellitePower::RestorePowerToFlightSystems();
+            } catch (etl::exception& e) {
+                MspException::LogException(e);
             }
         } else {
             Log_info0("Beacon is disabled, did not transmit");
