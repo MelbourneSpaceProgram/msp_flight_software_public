8ac57a2247d38e16aa7b10e1ed4c697aacb5925f
Benjamin Ding <ben@dingbenjamin.com>
2018-11-19 14:12:36 +1100

    Power cycle Telecomms after Lithium Write Flash

--- src/payload_processor/uplinks/lithium_write_flash_uplink.cpp
+++ src/payload_processor/uplinks/lithium_write_flash_uplink.cpp
@@ -1,8 +1,11 @@
 #include <src/messages/LithiumMd5Payload.pb.h>
 #include <src/payload_processor/uplinks/lithium_write_flash_uplink.h>
 #include <src/telecomms/lithium.h>
+#include <src/telecomms/lithium_commands/no_op_command.h>
 #include <src/telecomms/lithium_commands/write_flash_command.h>
 #include <src/util/nanopb_utils.h>
+#include <src/util/satellite_power.h>
+#include <src/util/task_utils.h>
 
 LithiumWriteFlashUplink::LithiumWriteFlashUplink(byte* payload)
     : Uplink(kLithiumWriteFlashUplinkArgumentLength),
@@ -10,5 +13,13 @@ LithiumWriteFlashUplink::LithiumWriteFlashUplink(byte* payload)
 
 bool LithiumWriteFlashUplink::ExecuteUplink() {
     WriteFlashCommand command(&md5);
-    return Lithium::GetInstance()->DoCommand(&command);
+    bool success = Lithium::GetInstance()->DoCommand(&command);
+
+    SatellitePower::CutPowerToTelecoms();
+    TaskUtils::SleepMilli(1000);
+    SatellitePower::RestorePowerToTelecoms();
+
+    NoOpCommand no_op;
+    success = success && Lithium::GetInstance()->DoCommand(&no_op);
+    return success;
 }
